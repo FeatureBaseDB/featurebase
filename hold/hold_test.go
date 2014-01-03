622b3b2351cd5dc8a2c0647d869e512759f03870
Cody Soyland <cody@soyland.org>
2014-01-03 17:27:19 -0600

    Add timeouts.

--- hold/hold_test.go
+++ hold/hold_test.go
@@ -11,17 +11,17 @@ import (
 func TestHoldChan(t *testing.T) {
 	Convey("set then get", t, func() {
 		id, _ := uuid.NewV4()
-		Hold.Set(id, "derp")
-		derp := Hold.Get(id)
+		Hold.Set(id, "derp", 10)
+		derp, _ := Hold.Get(id, 10)
 		So(derp, ShouldEqual, "derp")
 	})
 	Convey("get then set", t, func() {
 		id, _ := uuid.NewV4()
 		go func() {
 			time.Sleep(time.Second / 10)
-			Hold.Set(id, "derpsy")
+			Hold.Set(id, "derpsy", 10)
 		}()
-		derp := Hold.Get(id)
+		derp, _ := Hold.Get(id, 10)
 		So(derp, ShouldEqual, "derpsy")
 	})
 }
