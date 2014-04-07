a23055946bdcbf6a722c0cdf7db34c8eb3cd08bf
Todd Gruben <tgruben@umbel.com>
2014-04-07 17:32:59 -0500

    inc version number

--- core/service.go
+++ core/service.go
@@ -47,7 +47,7 @@ func NewService() *Service {
 	service.WebService = NewWebService(service)
 	service.Index = index.NewFragmentContainer()
 	service.Hold = hold.NewHolder()
-	service.version = "0.0.10"
+	service.version = "0.0.11"
 	service.name = "Cruncher"
 	service.PrepareLogging()
 	fmt.Printf("Pilosa %s\n", service.version)
