83a05152c36a559be82012aa6f0f512720762324
Todd Gruben <tgruben@umbel.com>
2014-04-17 03:13:06 -0500

    added profiling capability

--- commands/pilosa-cruncher/cruncher.go
+++ commands/pilosa-cruncher/cruncher.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"flag"
+	"log"
+	"os"
 	"pilosa/cruncher"
+	"runtime/pprof"
 )
 
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+
 func main() {
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
 	cruncher := cruncher.NewCruncher()
 	cruncher.Run()
 }
