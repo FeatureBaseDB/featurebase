a2b5396fa43c6a77e50d1b5586c7a5f4de776d48
Todd Gruben <tgruben@umbel.com>
2014-04-21 10:28:30 -0500

    added logging to diagnose hashring issues

--- db/topology.go
+++ db/topology.go
@@ -326,6 +326,7 @@ func (d *Database) GetFragmentForBitmap(slice *Slice, bitmap *Bitmap) (*Fragment
 	}
 	frag_id_s, err := fsi.hashring.Get(fmt.Sprintf("%d", bitmap.Id))
 	if err != nil {
+		log.Println("ERROR FSI.GET:", bitmap.Id)
 		log.Println(err)
 		return nil, err
 	}
