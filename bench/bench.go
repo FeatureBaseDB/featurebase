85b4e83c2dc95b6595dfa06b0ccd6ea211b7d0ef
jaffee <jaffee@umbel.com>
2016-11-15 13:01:31 -0600

    add MultiDBSetBits benchmark

--- bench/bench.go
+++ bench/bench.go
@@ -116,6 +116,70 @@ func (b *DiagonalSetBits) Run(agentNum int) map[string]interface{} {
 	return results
 }
 
+// MultiDBSetBits sets bits with increasing profile id and bitmap id.
+type MultiDBSetBits struct {
+	cli           *pilosa.Client
+	BaseBitmapID  int
+	BaseProfileID int
+	Iterations    int
+}
+
+func (b *MultiDBSetBits) Usage() string {
+	return `
+MultiDBSetBits sets bits with increasing profile id and bitmap id using a different DB for each agent.
+
+Usage: MultiDBSetBits [arguments]
+
+The following arguments are available:
+
+	-BaseBitmapID int
+		bits being set will all be greater than BaseBitmapID
+
+	-BaseProfileID int
+		profile id num to start from
+
+	-Iterations int
+		number of bits to set
+
+`[1:]
+}
+
+func (b *MultiDBSetBits) ConsumeFlags(args []string) ([]string, error) {
+	fs := flag.NewFlagSet("MultiDBSetBits", flag.ContinueOnError)
+	fs.SetOutput(ioutil.Discard)
+	fs.IntVar(&b.BaseBitmapID, "BaseBitmapID", 0, "")
+	fs.IntVar(&b.BaseProfileID, "BaseProfileID", 0, "")
+	fs.IntVar(&b.Iterations, "Iterations", 100, "")
+
+	if err := fs.Parse(args); err != nil {
+		return nil, err
+	}
+	return fs.Args(), nil
+}
+
+// Init connects to pilosa and sets the client on b.
+func (b *MultiDBSetBits) Init(hosts []string) (err error) {
+	b.cli, err = pilosa.NewClient(hosts[0])
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Run runs the MultiDBSetBits benchmark
+func (b *MultiDBSetBits) Run(agentNum int) map[string]interface{} {
+	results := make(map[string]interface{})
+	if b.cli == nil {
+		results["error"] = fmt.Errorf("No client set for MultiDBSetBits agent: %v", agentNum)
+		return results
+	}
+	for n := 0; n < b.Iterations; n++ {
+		query := fmt.Sprintf("SetBit(%d, 'frame.n', %d)", b.BaseBitmapID+n, b.BaseProfileID+n)
+		b.cli.ExecuteQuery("multidb"+strconv.Itoa(agentNum), query, true)
+	}
+	return results
+}
+
 // RandomSetBits sets bits randomly and deterministically based on a seed.
 type RandomSetBits struct {
 	cli            *pilosa.Client
@@ -297,6 +361,7 @@ func (sb *serialBenchmark) Init(hosts []string) error {
 func (sb *serialBenchmark) Run(agentNum int) map[string]interface{} {
 	results := make(map[string]interface{}, len(sb.benchmarkers))
 	runtimes := make(map[string]time.Duration)
+	total_start := time.Now()
 	for i, b := range sb.benchmarkers {
 		start := time.Now()
 		ret := b.Run(agentNum)
@@ -304,6 +369,7 @@ func (sb *serialBenchmark) Run(agentNum int) map[string]interface{} {
 		results[strconv.Itoa(i)] = ret
 		runtimes[strconv.Itoa(i)] = end.Sub(start)
 	}
+	runtimes["total"] = time.Now().Sub(total_start)
 	results["runtimes"] = runtimes
 	return results
 }
