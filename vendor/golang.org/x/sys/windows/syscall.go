23ab7063b8cbbc5b9e51fee1b38675e7668830e8
Cody Soyland <cody@soyland.org>
2016-11-29 12:36:58 -0600

    Switch to Glide.sh for dependency management (Fixes #145)

--- vendor/golang.org/x/sys/windows/syscall.go
+++ vendor/golang.org/x/sys/windows/syscall.go
@@ -2,12 +2,12 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build darwin dragonfly freebsd linux netbsd openbsd solaris
+// +build windows
 
-// Package unix contains an interface to the low-level operating system
+// Package windows contains an interface to the low-level operating system
 // primitives.  OS details vary depending on the underlying system, and
-// by default, godoc will display OS-specific documentation for the current
-// system.  If you want godoc to display OS documentation for another
+// by default, godoc will display the OS-specific documentation for the current
+// system.  If you want godoc to display syscall documentation for another
 // system, set $GOOS and $GOARCH to the desired system.  For example, if
 // you want to view documentation for freebsd/arm on linux/amd64, set $GOOS
 // to freebsd and $GOARCH to arm.
@@ -19,17 +19,19 @@
 // These calls return err == nil to indicate success; otherwise
 // err represents an operating system error describing the failure and
 // holds a value of type syscall.Errno.
-package unix // import "golang.org/x/sys/unix"
+package windows // import "golang.org/x/sys/windows"
 
-import "unsafe"
+import (
+	"syscall"
+)
 
 // ByteSliceFromString returns a NUL-terminated slice of bytes
 // containing the text of s. If s contains a NUL byte at any
-// location, it returns (nil, EINVAL).
+// location, it returns (nil, syscall.EINVAL).
 func ByteSliceFromString(s string) ([]byte, error) {
 	for i := 0; i < len(s); i++ {
 		if s[i] == 0 {
-			return nil, EINVAL
+			return nil, syscall.EINVAL
 		}
 	}
 	a := make([]byte, len(s)+1)
@@ -39,7 +41,7 @@ func ByteSliceFromString(s string) ([]byte, error) {
 
 // BytePtrFromString returns a pointer to a NUL-terminated array of
 // bytes containing the text of s. If s contains a NUL byte at any
-// location, it returns (nil, EINVAL).
+// location, it returns (nil, syscall.EINVAL).
 func BytePtrFromString(s string) (*byte, error) {
 	a, err := ByteSliceFromString(s)
 	if err != nil {
@@ -49,7 +51,7 @@ func BytePtrFromString(s string) (*byte, error) {
 }
 
 // Single-word zero for use when we need a valid pointer to 0 bytes.
-// See mkunix.pl.
+// See mksyscall.pl.
 var _zero uintptr
 
 func (ts *Timespec) Unix() (sec int64, nsec int64) {
@@ -67,10 +69,3 @@ func (ts *Timespec) Nano() int64 {
 func (tv *Timeval) Nano() int64 {
 	return int64(tv.Sec)*1e9 + int64(tv.Usec)*1000
 }
-
-func TimevalToNsec(tv Timeval) int64 { return int64(tv.Sec)*1e9 + int64(tv.Usec)*1e3 }
-
-// use is a no-op, but the compiler cannot see that it is.
-// Calling use(p) ensures that p is kept live until that point.
-//go:noescape
-func use(p unsafe.Pointer)
