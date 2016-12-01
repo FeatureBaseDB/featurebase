fe28a095a5137244724849c229926730b2c4da96
jaffee <jaffee@umbel.com>
2016-12-01 15:46:41 -0700

    add option to save all stats

--- bench/stats.go
+++ bench/stats.go
@@ -5,19 +5,25 @@ import (
 )
 
 type Stats struct {
-	Min   time.Duration
-	Max   time.Duration
-	Total time.Duration
-	Num   int64
+	Min     time.Duration
+	Max     time.Duration
+	Total   time.Duration
+	Num     int64
+	All     []time.Duration
+	SaveAll bool
 }
 
 func NewStats() *Stats {
 	return &Stats{
 		Min: 1<<63 - 1,
+		All: make([]time.Duration, 0),
 	}
 }
 
 func (s *Stats) Add(td time.Duration) {
+	if s.SaveAll {
+		s.All = append(s.All, td)
+	}
 	s.Num += 1
 	s.Total += td
 	if td < s.Min {
@@ -36,4 +42,7 @@ func AddToResults(s *Stats, results map[string]interface{}) {
 	results["min"] = s.Min
 	results["max"] = s.Max
 	results["avg"] = s.Avg()
+	if s.SaveAll {
+		results["all"] = s.All
+	}
 }
