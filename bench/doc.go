f4fd1083c845ecd02bfd1b5146b1e5d1a849f629
jaffee <jaffee@umbel.com>
2017-01-18 20:56:46 -0600

    tweaks to bench doc.go

--- bench/doc.go
+++ bench/doc.go
@@ -9,29 +9,31 @@
 // When writing a new benchmark, there are a few things to keep in mind other
 // than just implementing the interface:
 //
-// The benchmark should modify it's own configuration in its Init method based
+// 1. The benchmark should modify it's own configuration in its Init method based
 // on the agentNum it is given. How it modifies is specific to the benchmark,
 // but the idea is that it should make sense to call the benchmark with the same
 // configuration, but multiple different agent numbers, and it should do useful
 // work each time (i.e. not just setting the same bits, or running the same
 // queries).
 //
-// The Init method should do everything that needs to be done to get the
+// 2. The Init method should do everything that needs to be done to get the
 // benchmark to a runnable state - all code in run should be the stuff that we
 // actually want to time.
 //
-// The Run method does not need to report the total runtime - that is collected
+// 3. The Run method does not need to report the total runtime - that is collected
 // by calling code.
 //
-// Usage should follow the format in other benchmarks, and explain how the
+// 4. Usage should follow the format in other benchmarks, and explain how the
 // benchmark uses agentNum to modify its behavior
 //
 //
 // Files:
 //
 // 1. client.go contains pilosa client code which is shared by many benchmarks
+//
 // 2. errgroup.go contains the ErrGroup implementation copied from golang.org/x/
 // so as not to pull in a bunch of useless deps.
+//
 // 3. stats.go contains useful code for gathering stats about a series of timed
 // operations.
 package bench
