8ddff3235f5c64906c7558229e8cb7dec51d1cd7
Linh Vo <lvo@umbel.com>
2017-01-25 00:14:20 -0600

    add pql option to randquery

--- bench/randquery.go
+++ bench/randquery.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"strings"
 	"time"
+	"errors"
 )
 
 // RandomQuery queries randomly and deterministically based on a seed.
@@ -67,6 +68,9 @@ The following arguments are available:
 
 	-client-type string
 		Can be 'single' (all agents hitting one host) or 'round_robin'
+
+	-content-type string
+		protobuf or pql
 `[1:]
 }
 
@@ -86,6 +90,7 @@ func (b *RandomQuery) ConsumeFlags(args []string) ([]string, error) {
 	var dbs string
 	fs.StringVar(&dbs, "dbs", "benchdb", "")
 	fs.StringVar(&b.ClientType, "client-type", "single", "")
+	fs.StringVar(&b.ContentType, "content-type", "protobuf", "")
 
 	if err := fs.Parse(args); err != nil {
 		return nil, err
@@ -107,9 +112,20 @@ func (b *RandomQuery) Run(ctx context.Context) map[string]interface{} {
 	for n := 0; n < b.Iterations; n++ {
 		call := qm.Random(b.MaxN, b.MaxDepth, b.MaxArgs, uint64(b.BaseBitmapID), uint64(b.BitmapIDRange))
 		start = time.Now()
-		b.client.ExecuteQuery(ctx, b.DBs[n%len(b.DBs)], call.String(), true)
+		b.ExecuteQuery(b.ContentType, b.DBs[n % len(b.DBs)], call.String(), ctx)
 		s.Add(time.Now().Sub(start))
 	}
 	AddToResults(s, results)
 	return results
+}
+
+func (b *RandomQuery) ExecuteQuery(contentType, db, query string, ctx context.Context, ) (interface{}, error) {
+	if contentType == "protobuf" {
+		return b.client.ExecuteQuery(ctx, db, query, true)
+	} else if contentType == "pql" {
+		fmt.Println("HERRR")
+		return b.client.ExecutePql(ctx, db, query)
+	} else {
+		return nil, errors.New("unsupport content type")
+	}
 }
