df0327f0f2c5e6ef09b76c18e9ea94c61b9264db
Matt Jaffee <jaffee@umbel.com>
2017-03-15 21:28:44 -0500

    rename cmd.Serve->cmd.Server (gorename)

--- cmd/server.go
+++ cmd/server.go
@@ -14,11 +14,11 @@ import (
 )
 
 // Serve is global so that tests can control and verify it.
-var Serve *server.Command
+var Server *server.Command
 
 func NewServeCmd(stdin io.Reader, stdout, stderr io.Writer) *cobra.Command {
-	Serve = server.NewCommand()
-	Serve.Stdin, Serve.Stdout, Serve.Stderr = stdin, stdout, stderr
+	Server = server.NewCommand()
+	Server.Stdin, Server.Stdout, Server.Stderr = stdin, stdout, stderr
 	serveCmd := &cobra.Command{
 		Use:   "server",
 		Short: "Run Pilosa.",
@@ -28,28 +28,28 @@ It will load existing data from the configured
 directory, and start listening client connections
 on the configured port.`,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			Serve.Server.Handler.Version = Version
-			fmt.Fprintf(Serve.Stderr, "Pilosa %s, build time %s\n", Version, BuildTime)
+			Server.Server.Handler.Version = Version
+			fmt.Fprintf(Server.Stderr, "Pilosa %s, build time %s\n", Version, BuildTime)
 
 			// Start CPU profiling.
-			if Serve.CPUProfile != "" {
-				f, err := os.Create(Serve.CPUProfile)
+			if Server.CPUProfile != "" {
+				f, err := os.Create(Server.CPUProfile)
 				if err != nil {
 					return fmt.Errorf("create cpu profile: %v", err)
 				}
 				defer f.Close()
 
-				fmt.Fprintln(Serve.Stderr, "Starting cpu profile")
+				fmt.Fprintln(Server.Stderr, "Starting cpu profile")
 				pprof.StartCPUProfile(f)
-				time.AfterFunc(Serve.CPUTime, func() {
-					fmt.Fprintln(Serve.Stderr, "Stopping cpu profile")
+				time.AfterFunc(Server.CPUTime, func() {
+					fmt.Fprintln(Server.Stderr, "Stopping cpu profile")
 					pprof.StopCPUProfile()
 					f.Close()
 				})
 			}
 
 			// Execute the program.
-			if err := Serve.Run(); err != nil {
+			if err := Server.Run(); err != nil {
 				return err
 			}
 
@@ -58,32 +58,32 @@ on the configured port.`,
 			signal.Notify(c, os.Interrupt)
 			select {
 			case sig := <-c:
-				fmt.Fprintf(Serve.Stderr, "Received %s; gracefully shutting down...\n", sig.String())
+				fmt.Fprintf(Server.Stderr, "Received %s; gracefully shutting down...\n", sig.String())
 
 				// Second signal causes a hard shutdown.
 				go func() { <-c; os.Exit(1) }()
 
-				if err := Serve.Close(); err != nil {
+				if err := Server.Close(); err != nil {
 					return err
 				}
-			case <-Serve.Done:
-				fmt.Fprintf(Serve.Stderr, "Server closed externally")
+			case <-Server.Done:
+				fmt.Fprintf(Server.Stderr, "Server closed externally")
 			}
 			return nil
 		},
 	}
 	flags := serveCmd.Flags()
 
-	flags.StringVarP(&Serve.ConfigPath, "config", "c", "", "Configuration file to read from.")
-	flags.StringVarP(&Serve.Config.DataDir, "data-dir", "d", "~/.pilosa", "Directory to store pilosa data files.")
-	flags.StringVarP(&Serve.Config.Host, "bind", "", ":10101", "Default URI on which pilosa should listen.")
-	flags.IntVarP(&Serve.Config.Cluster.ReplicaN, "cluster.replicas", "", 1, "Number hosts each piece of data should be stored on.")
-	flags.StringSliceVarP(&Serve.Config.Cluster.Nodes, "cluster.hosts", "", []string{}, "Comma separated list of hosts in cluster.")
-	flags.DurationVarP((*time.Duration)(&Serve.Config.Cluster.PollingInterval), "cluster.poll-interval", "", time.Minute, "Polling interval for cluster.") // TODO what actually is this?
-	flags.StringVarP(&Serve.Config.Plugins.Path, "plugins.path", "", "", "Path to plugin directory.")
-	flags.DurationVarP((*time.Duration)(&Serve.Config.AntiEntropy.Interval), "anti-entropy.interval", "", time.Minute*10, "Interval at which to run anti-entropy routine.")
-	flags.StringVarP(&Serve.CPUProfile, "profile.cpu", "", "", "Where to store CPU profile.")
-	flags.DurationVarP(&Serve.CPUTime, "profile.cpu-time", "", 30*time.Second, "CPU profile duration.")
+	flags.StringVarP(&Server.ConfigPath, "config", "c", "", "Configuration file to read from.")
+	flags.StringVarP(&Server.Config.DataDir, "data-dir", "d", "~/.pilosa", "Directory to store pilosa data files.")
+	flags.StringVarP(&Server.Config.Host, "bind", "", ":10101", "Default URI on which pilosa should listen.")
+	flags.IntVarP(&Server.Config.Cluster.ReplicaN, "cluster.replicas", "", 1, "Number hosts each piece of data should be stored on.")
+	flags.StringSliceVarP(&Server.Config.Cluster.Nodes, "cluster.hosts", "", []string{}, "Comma separated list of hosts in cluster.")
+	flags.DurationVarP((*time.Duration)(&Server.Config.Cluster.PollingInterval), "cluster.poll-interval", "", time.Minute, "Polling interval for cluster.") // TODO what actually is this?
+	flags.StringVarP(&Server.Config.Plugins.Path, "plugins.path", "", "", "Path to plugin directory.")
+	flags.DurationVarP((*time.Duration)(&Server.Config.AntiEntropy.Interval), "anti-entropy.interval", "", time.Minute*10, "Interval at which to run anti-entropy routine.")
+	flags.StringVarP(&Server.CPUProfile, "profile.cpu", "", "", "Where to store CPU profile.")
+	flags.DurationVarP(&Server.CPUTime, "profile.cpu-time", "", 30*time.Second, "CPU profile duration.")
 
 	return serveCmd
 }
