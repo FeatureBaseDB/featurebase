7321a208a89160242767028c27ef5cf669ef150f
Matt Jaffee <jaffee@umbel.com>
2017-03-15 22:29:44 -0500

    put all cmd test helpers in root_test and return errors from them

--- cmd/inspect_test.go
+++ cmd/inspect_test.go
@@ -6,9 +6,23 @@ import (
 )
 
 func TestInspectHelp(t *testing.T) {
-	output := ExecNewRootCommand(t, "inspect", "--help")
+	output, err := ExecNewRootCommand(t, "inspect", "--help")
 	if !strings.Contains(output, "Usage:") ||
-		!strings.Contains(output, "pilosa inspect") {
-		t.Fatalf("Command 'inspect --help' not working, got: %s", output)
+		!strings.Contains(output, "pilosa inspect") || err != nil {
+		t.Fatalf("Command 'inspect --help' not working, err: '%v', output: '%s'", err, output)
+	}
+}
+
+func TestInspectNoPath(t *testing.T) {
+	output, err := ExecNewRootCommand(t, "inspect")
+	if !strings.Contains(err.Error(), "path required") {
+		t.Fatalf("Command 'inspect' without args should error but: err: '%v', output: '%v'", err, output)
+	}
+}
+
+func TestInspectMultiPath(t *testing.T) {
+	output, err := ExecNewRootCommand(t, "inspect", "one", "two")
+	if !strings.Contains(err.Error(), "only one path") {
+		t.Fatalf("Command 'inspect' without args should error but: err: '%v', output: '%v'", err, output)
 	}
 }
