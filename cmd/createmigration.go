e41975ff2fc320769f68056d110a0e7b1307548c
Todd Gruben <tgruben@umbel.com>
2017-03-20 16:39:13 -0500

    made configuration options consistent with struct values

--- cmd/createmigration.go
+++ cmd/createmigration.go
@@ -27,7 +27,7 @@ func NewMigrationCmd(stdin io.Reader, stdout, stderr io.Writer) *cobra.Command {
 	flags.StringVarP(&MigrationPlanner.Host, "host", "p", "localhost:15000", "host:port of Pilosa providing the schema.")
 	flags.StringVarP(&MigrationPlanner.SrcConfig, "src-config", "s", "", "Original Cluster Node Configuration File")
 	flags.StringVarP(&MigrationPlanner.DestConfig, "dest-config", "d", "", "Proposed Cluster Node Configuration File")
-	flags.StringVarP(&MigrationPlanner.DestFileName, "output-file", "o", "-", "filename to store the plan")
+	flags.StringVarP(&MigrationPlanner.OutputFileName, "output-file", "o", "-", "filename to store the plan")
 	return migrationCmd
 }
 
--- ctl/createMigration.go
+++ ctl/createMigration.go
@@ -4,18 +4,15 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
-	"fmt"
 	"io"
-	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
-	"sort"
 	"strconv"
 	"strings"
 
 	"github.com/BurntSushi/toml"
 	"github.com/pilosa/pilosa"
+	"github.com/pilosa/pilosa/internal"
 )
 
 // BackupCommand represents a command for backing up a frame.
@@ -27,7 +24,7 @@ type CreateMigrationCommand struct {
 	SrcConfig  string
 	DestConfig string
 
-	DestFileName string
+	OutputFileName string
 
 	// Standard input/output
 	Stdin  io.Reader
@@ -45,7 +42,33 @@ func NewCreateMigrationCommand(stdin io.Reader, stdout, stderr io.Writer) *Creat
 
 func (cmd *CreateMigrationCommand) Run(ctx context.Context) error {
 
-	plan := BuildPlan(cmd.Host, cmd.SrcConfig, cmd.DestConfig)
+	client, err := pilosa.NewClient(cmd.Host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	schema, err := client.Schema(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	srcConfig := pilosa.NewConfig()
+	destConfig := pilosa.NewConfig()
+
+	if _, err := toml.DecodeFile(cmd.SrcConfig, srcConfig); err != nil {
+		log.Fatal(err)
+	}
+	if _, err := toml.DecodeFile(cmd.DestConfig, destConfig); err != nil {
+		log.Fatal(err)
+	}
+
+	srcCluster := srcConfig.PilosaCluster()
+	destCluster := destConfig.PilosaCluster()
+
+	plan := NewMigrationPlan(srcConfig.DataDir, destConfig.DataDir)
+
+	BuildPlan(schema, srcCluster, destCluster, plan)
+
 	//either write it to a file or stdout
 	if cmd.DestFileName == "-" {
 		json.NewEncoder(os.Stdout).Encode(plan)
@@ -74,36 +97,6 @@ func NewMigrationPlan(srcpath, destpath string) *Plan {
 		DestDataDir: destpath}
 }
 
-type MaxSliceResponse struct {
-	MaxSlices map[string]uint64 `json:"MaxSlices"`
-}
-
-type frame struct {
-	Name string `json:"name"`
-}
-
-type db struct {
-	Frames []frame `json:"frames"`
-	Name   string  `json:"name"`
-}
-
-type SchemaResponse struct {
-	Dbs []db `json:"dbs"`
-}
-
-func getFrames(s SchemaResponse, dbv string) []string {
-	var ret []string
-	for _, db := range s.Dbs {
-		if db.Name == dbv {
-			for _, v := range db.Frames {
-				ret = append(ret, v.Name)
-			}
-			return ret
-		}
-	}
-	return ret
-}
-
 func make_key(db string, slice uint64, host string) string {
 	var buffer bytes.Buffer
 	buffer.WriteString(db)
@@ -129,162 +122,32 @@ func clean(host string) string {
 	return host
 }
 
-func dumpNarrative(d map[string][]*Destination, srcConfig, destConfig *pilosa.Config) {
-	for srchost, v := range d {
-		fmt.Println("# GOTO host:", srchost)
-		remfiles := make(map[string]struct{})
-		for _, part := range v {
-			destBase := destConfig.DataDir + "/" + part.Db + "/" + part.Frame
-			fmt.Printf("#make remote directory if doesn't exist %s:%s\n", part.Host, destBase)
-			srcBase := srcConfig.DataDir + "/" + part.Db + "/" + part.Frame + "/" + strconv.FormatUint(part.Slice, 10)
-			destBase = destBase + "/" + strconv.FormatUint(part.Slice, 10)
-			fmt.Printf("# copy local file %s to %s:%s\n", srcBase, part.Host, destBase)
-			remfiles[srcBase] = struct{}{}
-			//fmt.Printf("# remove local file %s \n", srcBase)
-			srcBase += ".cache"
-			destBase += ".cache"
-			fmt.Printf("# copy local file %s to %s:%s\n", srcBase, part.Host, destBase)
-			remfiles[srcBase] = struct{}{}
-		}
-		for f, _ := range remfiles {
-			fmt.Printf("# remove local file %s \n", f)
-		}
-		fmt.Println("# DONE host:", srchost)
-		fmt.Println("")
-
-	}
-
-}
-
-func dumpClusterState(state map[string]struct{}, out io.Writer) {
-	results := make([]struct {
-		Db    string `json:db`
-		Slice int    `json:slice`
-		Host  string `json:host`
-	}, 0, len(state))
-	for k, _ := range state {
-		p := strings.Split(k, "|")
-		v, _ := strconv.Atoi(p[1])
-		results = append(results, struct {
-			Db    string `json:db`
-			Slice int    `json:slice`
-			Host  string `json:host`
-		}{p[0], v, p[2]})
-	}
-	json.NewEncoder(out).Encode(results)
-}
-
-func diff(X, Y []string) []string {
-	difference := make([]string, 0)
-	var i, j int
-	for i < len(X) && j < len(Y) {
-		if X[i] < Y[j] {
-			difference = append(difference, X[i])
-			i++
-		} else if X[i] > Y[j] {
-			j++
-		} else { //X[i] == Y[j]
-			j++
-			i++
-		}
-	}
-	if i < len(X) { //All remaining in X are greater than Y, just copy over
-		finalLength := len(X) - i + len(difference)
-		if finalLength > cap(difference) {
-			newDifference := make([]string, finalLength)
-			copy(newDifference, difference)
-			copy(newDifference[len(difference):], X[i:])
-			difference = newDifference
-		} else {
-			differenceLen := len(difference)
-			difference = difference[:finalLength]
-			copy(difference[differenceLen:], X[i:])
-		}
-	}
-	return difference
-}
-
-func sortKeys(m map[string]struct{}) []string {
-	keys := make([]string, len(m), len(m))
-	for k, _ := range m {
-		keys = append(keys, k)
-	}
-	sort.Strings(keys)
-	return keys
-}
-
-func BuildPlan(server, srcConfigurationPath, destConfigurationPath string) *Plan {
-
-	var m MaxSliceResponse
-	var s SchemaResponse
-
-	// Fetch /slices/max
-	response, err := http.Get("http://" + server + "/slices/max")
-	if err != nil {
-		log.Fatal(err)
-	} else {
-		defer response.Body.Close()
-		responseData, err := ioutil.ReadAll(response.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if err := json.Unmarshal(responseData, &m); err != nil {
-			log.Fatal(err)
-		}
-	}
-	// fetch /schema
-	response, err = http.Get("http://" + server + "/schema")
-	if err != nil {
-		log.Fatal(err)
-	} else {
-		defer response.Body.Close()
-		responseData, err := ioutil.ReadAll(response.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if err := json.Unmarshal(responseData, &s); err != nil {
-			log.Fatal(err)
-		}
-	}
-	srcConfig := pilosa.NewConfig()
-	destConfig := pilosa.NewConfig()
-
-	if _, err := toml.DecodeFile(srcConfigurationPath, srcConfig); err != nil {
-		log.Fatal(err)
-	}
-	if _, err := toml.DecodeFile(destConfigurationPath, destConfig); err != nil {
-		log.Fatal(err)
-	}
-
-	srcCluster := srcConfig.PilosaCluster()
-	destCluster := destConfig.PilosaCluster()
+func BuildPlan(schema []*internal.DB, srcCluster, destCluster *pilosa.Cluster, plan *Plan) {
 
 	srcLookup := make(map[string]struct{})
 	destLookup := make(map[string]struct{})
 
-	for db, maxs := range m.MaxSlices {
-		for slice := uint64(0); slice <= maxs; slice++ {
-			for _, src := range srcCluster.FragmentNodes(db, slice) {
-				key := make_key(db, slice, clean(src.Host))
+	for _, dbinfo := range schema {
+		for slice := uint64(0); slice <= dbinfo.MaxSlice; slice++ {
+			for _, src := range srcCluster.FragmentNodes(dbinfo.Name, slice) {
+				key := make_key(dbinfo.Name, slice, clean(src.Host))
 				srcLookup[key] = struct{}{}
 			}
 		}
 	}
-	plan := NewMigrationPlan(srcConfig.DataDir, destConfig.DataDir)
-	for db, maxs := range m.MaxSlices {
-		frames := getFrames(s, db)
-		for slice := uint64(0); slice <= maxs; slice++ {
-			for _, src := range srcCluster.FragmentNodes(db, slice) {
-				for _, dest := range destCluster.FragmentNodes(db, slice) {
-					key := make_key(db, slice, clean(dest.Host))
+	for _, dbinfo := range schema {
+		for slice := uint64(0); slice <= dbinfo.MaxSlice; slice++ {
+			for _, src := range srcCluster.FragmentNodes(dbinfo.Name, slice) {
+				for _, dest := range destCluster.FragmentNodes(dbinfo.Name, slice) {
+					key := make_key(dbinfo.Name, slice, clean(dest.Host))
 					destLookup[key] = struct{}{}
 					_, found := srcLookup[key]
 					if !found {
-						for _, frame := range frames {
+						for _, frame := range dbinfo.Frames {
 							plan.Actions[src.Host] = append(plan.Actions[src.Host],
 								&Destination{
-									Db:    db,
-									Frame: frame,
+									Db:    dbinfo.Name,
+									Frame: frame.Name,
 									Slice: slice,
 									Host:  clean(dest.Host),
 								})
@@ -295,5 +158,4 @@ func BuildPlan(server, srcConfigurationPath, destConfigurationPath string) *Plan
 			}
 		}
 	}
-	return plan
 }
