0a324d14a5869c4efa09635346eb9bf354cd1382
Matt Jaffee <jaffee@umbel.com>
2017-04-18 14:11:42 -0500

    add fix all package level docs

---
+++ cmd/doc.go
@@ -0,0 +1,4 @@
+/*
+Package cmd contains all the pilosa subcommand definitions (1 per file).
+*/
+package cmd
---
+++ ctl/doc.go
@@ -0,0 +1,3 @@
+// package ctl contains all pilosa subcommands other than 'server'. These are
+// generally administration, testing, and debugging tools.
+package ctl
