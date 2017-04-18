10d78f9fd62f3505b22b8127ca6005a14566d4a1
Matt Jaffee <jaffee@umbel.com>
2017-04-18 13:55:48 -0500

    add top level doc.go file to pilosa package

---
+++ doc.go
@@ -0,0 +1,9 @@
+/*
+
+Package pilosa implements the core of the Pilosa distributed bitmap index. It
+contains all the core domain objects, interfaces, and high level logic that make
+pilosa work.
+
+*/
+
+package pilosa
