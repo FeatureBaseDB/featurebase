2930101aa7b174446de765c834866c2c7a1e4d18
Todd Gruben <tgruben@umbel.com>
2017-04-27 15:51:26 -0500

    added merge top plugin

---
+++ plugins/merge.go
@@ -0,0 +1,69 @@
+package plugins
+
+import (
+	"context"
+
+	"github.com/pilosa/pilosa"
+)
+
+func init() {
+	pilosa.RegisterPlugin("MergeTop", NewMergeTopPlugin)
+}
+
+// MergeTopPlugin represents a plugin that will print args to stderr.
+type MergeTopPlugin struct {
+	holder *pilosa.Holder
+}
+
+// NewMergeTopPlugin returns a new instance of MergeTopPlugin.
+func NewMergeTopPlugin(h *pilosa.Holder) pilosa.Plugin {
+	return &MergeTopPlugin{h}
+}
+
+// Map executes the plugin against a single slice.
+func (p *MergeTopPlugin) Map(ctx context.Context, index string, children []interface{}, args map[string]interface{}, slice uint64) (interface{}, error) {
+	n := 0
+	var frame string
+
+	if x, found := args["n"]; found {
+		n = x.(int)
+	}
+
+	if fr, found := args["frame"]; found {
+		frame = fr.(string)
+	} else {
+		//error if no frame label is given
+	}
+
+	view := p.holder.View(index, frame, pilosa.ViewStandard)
+	f := view.Fragment(slice)
+
+	toplist, err := f.Top(pilosa.TopOptions{N: n})
+	if err != nil {
+		return nil, err
+	}
+
+	bm := pilosa.NewBitmap()
+	for _, pair := range toplist {
+		x := f.Row(pair.ID)
+		bm = bm.Union(x)
+
+	}
+	return bm, nil
+}
+
+// Reduce combines previous map results into a single value.
+func (p *MergeTopPlugin) Reduce(ctx context.Context, prev, v interface{}) interface{} {
+	switch x := v.(type) {
+	case *pilosa.Bitmap:
+		if prev != nil {
+			bm := prev.(*pilosa.Bitmap)
+			return bm.Union(x)
+		}
+		return x
+	case int:
+		return x
+	}
+
+	return v
+}
