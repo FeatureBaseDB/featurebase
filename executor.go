d4ddf3676c8f5ff2125b6c3a536e2ab829a9f443
Ben Johnson <benbjohnson@yahoo.com>
2017-05-07 15:21:30 -0600

    Support inverse Range() queries.

--- executor.go
+++ executor.go
@@ -502,17 +502,41 @@ func (e *Executor) executeRangeSlice(ctx context.Context, index string, c *pql.C
 		frame = DefaultFrame
 	}
 
+	// Retrieve column label.
+	idx := e.Holder.Index(index)
+	if idx == nil {
+		return nil, ErrIndexNotFound
+	}
+	columnLabel := idx.ColumnLabel()
+
 	// Retrieve base frame.
-	f := e.Holder.Frame(index, frame)
+	f := idx.Frame(frame)
 	if f == nil {
 		return nil, ErrFrameNotFound
 	}
 	rowLabel := f.RowLabel()
 
-	// Read row id.
-	rowID, _, err := c.UintArg(rowLabel) // TODO: why are we ignoring missing rowID?
+	// Read row & column id.
+	columnID, columnOK, err := c.UintArg(columnLabel)
+	if err != nil {
+		return nil, fmt.Errorf("executeRangeSlice - reading column: %v", err)
+	}
+	rowID, rowOK, err := c.UintArg(rowLabel)
 	if err != nil {
 		return nil, fmt.Errorf("executeRangeSlice - reading row: %v", err)
+	}
+
+	// Determine view.
+	var id uint64
+	var viewName string
+	if columnOK && rowOK {
+		return nil, fmt.Errorf("Range() cannot contain both %q and %q", columnLabel, rowLabel)
+	} else if !columnOK && !rowOK {
+		return nil, fmt.Errorf("Range() must specify either %q or %q", columnLabel, rowLabel)
+	} else if columnOK {
+		viewName, id = ViewInverse, columnID
+	} else {
+		viewName, id = ViewStandard, rowID
 	}
 
 	// Parse start time.
@@ -543,12 +567,12 @@ func (e *Executor) executeRangeSlice(ctx context.Context, index string, c *pql.C
 
 	// Union bitmaps across all time-based subframes.
 	bm := &Bitmap{}
-	for _, view := range ViewsByTimeRange(ViewStandard, startTime, endTime, q) {
+	for _, view := range ViewsByTimeRange(viewName, startTime, endTime, q) {
 		f := e.Holder.Fragment(index, frame, view, slice)
 		if f == nil {
 			continue
 		}
-		bm = bm.Union(f.Row(rowID))
+		bm = bm.Union(f.Row(id))
 	}
 	return bm, nil
 }
