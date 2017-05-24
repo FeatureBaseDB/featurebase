da961be6144c444e0e3c40ef099ff8968bcda6b7
Matt Jaffee <jaffee@umbel.com>
2017-05-24 12:26:52 -0500

    rewrite intersectCountArrayBitmap for perf test

--- roaring/roaring_internal_test.go
+++ roaring/roaring_internal_test.go
@@ -14,9 +14,7 @@
 
 package roaring
 
-import (
-	"testing"
-)
+import "testing"
 
 func TestBitmapCountRange(t *testing.T) {
 	c := container{}
@@ -38,6 +36,51 @@ func TestBitmapCountRange(t *testing.T) {
 		c.bitmap = test.bitmap
 		if ret := c.bitmapCountRange(test.start, test.end); ret != test.exp {
 			t.Fatalf("test #%v count of %v from %v to %v should be %v but got %v", i, test.bitmap, test.start, test.end, test.exp, ret)
+		}
+	}
+}
+
+func TestIntersectionCountArrayBitmap2(t *testing.T) {
+	a, b := &container{}, &container{}
+	tests := []struct {
+		array  []uint32
+		bitmap []uint64
+		exp    uint64
+	}{
+		{
+			array:  []uint32{0},
+			bitmap: []uint64{1},
+			exp:    1,
+		},
+		{
+			array:  []uint32{0, 1},
+			bitmap: []uint64{3},
+			exp:    2,
+		},
+		{
+			array:  []uint32{64, 128, 129, 2000},
+			bitmap: []uint64{932421, 2},
+			exp:    0,
+		},
+		{
+			array:  []uint32{0, 65, 130, 195},
+			bitmap: []uint64{255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255},
+			exp:    4,
+		},
+		{
+			array:  []uint32{63, 120, 543, 639, 12000},
+			bitmap: []uint64{0x8000000000000000, 0, 0, 0, 0, 0, 0, 0, 0, 0x8000000000000000},
+			exp:    2,
+		},
+	}
+
+	for i, test := range tests {
+		a.array = test.array
+		b.bitmap = test.bitmap
+		ret1 := intersectionCountArrayBitmapOld(a, b)
+		ret2 := intersectionCountArrayBitmap(a, b)
+		if ret1 != ret2 || ret2 != test.exp {
+			t.Fatalf("test #%v intersectCountArrayBitmap fail orig: %v new: %v exp: %v", i, ret1, ret2, test.exp)
 		}
 	}
 }
