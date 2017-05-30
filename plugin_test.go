23d7b0521349eafda0c9e3431a3e861fa6aa8e74
Yuce Tekol <yucetekol@gmail.com>
2017-05-30 20:00:19 +0300

    fixed plugin tests

--- plugin_test.go
+++ plugin_test.go
@@ -4,11 +4,12 @@ import (
 	"context"
 
 	"github.com/pilosa/pilosa"
+	"github.com/pilosa/pilosa/pql"
 )
 
 // MockPlugin represents a plugin that is implemented as mockable functions.
 type MockPlugin struct {
-	MapFn    func(ctx context.Context, db string, children []interface{}, args map[string]interface{}, slice uint64) (interface{}, error)
+	MapFn    func(ctx context.Context, index string, call *pql.Call, slice uint64) (interface{}, error)
 	ReduceFn func(ctx context.Context, prev, v interface{}) interface{}
 }
 
@@ -16,12 +17,12 @@ type MockPluginConstructorWrapper struct {
 	mock *MockPlugin
 }
 
-func (m *MockPluginConstructorWrapper) NewMockPluginConstruct(h *pilosa.Holder) pilosa.Plugin {
+func (m *MockPluginConstructorWrapper) NewMockPluginConstruct(e *pilosa.Executor) pilosa.Plugin {
 	return m.mock
 }
 
-func (p *MockPlugin) Map(ctx context.Context, db string, children []interface{}, args map[string]interface{}, slice uint64) (interface{}, error) {
-	return p.MapFn(ctx, db, children, args, slice)
+func (p *MockPlugin) Map(ctx context.Context, index string, call *pql.Call, slice uint64) (interface{}, error) {
+	return p.MapFn(ctx, index, call, slice)
 }
 
 func (p *MockPlugin) Reduce(ctx context.Context, prev, v interface{}) interface{} {
