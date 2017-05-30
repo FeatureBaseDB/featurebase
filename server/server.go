d2fbc0a08ff1a8a12768b0550b837610265e864d
Michael Baird <mbaird@umbel.com>
2017-05-30 16:33:03 -0500

    set long query time parameter at startup

--- server/server.go
+++ server/server.go
@@ -198,6 +198,7 @@ func (m *Command) SetupServer() error {
 
 	// Set configuration options.
 	m.Server.AntiEntropyInterval = time.Duration(m.Config.AntiEntropy.Interval)
+	m.Server.Cluster.LongQueryTime = time.Duration(m.Config.Cluster.LongQueryTime)
 	return nil
 }
 
