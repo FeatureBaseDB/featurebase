5ce6b5d684fe8c6f332c159f9dc3d199e7268b68
Linh Vo <lvo@umbel.com>
2017-06-13 10:14:14 -0500

    CreateInputDefinition tests

--- index_test.go
+++ index_test.go
@@ -258,3 +258,32 @@ func TestIndex_CreateInputDefinition(t *testing.T) {
 		t.Fatalf("unexpected input definition actions", inputDef.Fields())
 	}
 }
+
+func TestIndex_CreateExistingInputDefinition(t *testing.T) {
+	index := MustOpenIndex()
+	defer index.Close()
+
+	// Create Input Definition.
+	frames := pilosa.InputFrame{Name: "f", Options: pilosa.FrameOptions{RowLabel: "row"}}
+	action := pilosa.Action{Frame: "f", ValueDestination: "map", ValueMap: map[string]uint64{"Green": 1}}
+	fields := pilosa.Field{Name: "id", PrimaryKey: true, Actions: []pilosa.Action{action}}
+	_ , err := index.CreateInputDefinition("test", []pilosa.InputFrame{frames}, []pilosa.Field{fields})
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = index.CreateInputDefinition("test", []pilosa.InputFrame{frames}, []pilosa.Field{fields})
+	if err != pilosa.ErrInputDefinitionExists {
+		t.Fatal(err)
+	}
+}
+
+func TestIndex_CreateEmptyInputDefinition(t *testing.T) {
+	index := MustOpenIndex()
+	defer index.Close()
+
+	// Create Input Definition.
+	_ , err := index.CreateInputDefinition("test", []pilosa.InputFrame{}, []pilosa.Field{})
+	if err.Error() != "frames and fields are required" {
+		t.Fatal(err)
+	}
+}
