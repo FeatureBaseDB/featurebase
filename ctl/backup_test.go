e1f44d4e319c2f1d2b267aa85aaff523ef2f1394
Alan Bernstein <alan.aaron.bernstein@gmail.com>
2017-06-26 11:23:37 -0500

    Update tests

--- ctl/backup_test.go
+++ ctl/backup_test.go
@@ -23,6 +23,7 @@ import (
 	"io/ioutil"
 	"net/http/httptest"
 	"net/url"
+	"os"
 	"testing"
 )
 
@@ -190,7 +191,7 @@ func NewHolder() *Holder {
 // MustOpenHolder creates and opens a holder at a temporary path. Panic on error.
 func MustOpenHolder() *Holder {
 	h := NewHolder()
-	if err := h.Open(); err != nil {
+	if err := h.Open(os.Stderr); err != nil {
 		panic(err)
 	}
 	return h
