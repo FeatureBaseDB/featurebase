89b72cbfc8eae946fde9c8ba873644de794134e3
Todd Gruben <tgruben@umbel.com>
2017-08-15 12:33:19 -0500

    provided some clarification of CountRange issue

--- roaring/roaring.go
+++ roaring/roaring.go
@@ -234,6 +234,8 @@ func (b *Bitmap) CountRange(start, end uint64) (n uint64) {
 
 	// Count containers in between.
 	for x := i + 1; x < j; x++ {
+		// Ensure that current container is inside the requested range since
+		// keys to not have to be consecutive
 		if ekey < b.keys[x] {
 			break
 		}
