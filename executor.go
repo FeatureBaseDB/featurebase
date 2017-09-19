8a247cf5ea426b30ac9e06b5c012bc4789e9b202
Matthew Jaffee <jaffee@pilosa.com>
2017-09-19 12:39:36 -0500

    remove slice parallelism in mapperLocal

--- executor.go
+++ executor.go
@@ -1554,17 +1554,13 @@ func (e *Executor) mapperLocal(ctx context.Context, slices []uint64, mapFn mapFu
 	ch := make(chan mapResponse, len(slices))
 
 	for _, slice := range slices {
-		e.Holder.logger().Printf("launching gr for slice: %v", slice)
-		go func(slice uint64) {
-			e.Holder.logger().Printf("started gr for slice: %v", slice)
-			result, err := mapFn(slice)
+		result, err := mapFn(slice)
 
-			// Return response to the channel.
-			select {
-			case <-ctx.Done():
-			case ch <- mapResponse{result: result, err: err}:
-			}
-		}(slice)
+		// Return response to the channel.
+		select {
+		case <-ctx.Done():
+		case ch <- mapResponse{result: result, err: err}:
+		}
 	}
 
 	// Reduce results
