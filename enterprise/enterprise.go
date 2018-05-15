4f3a4864f0e361f4d645bd9639b9c8851073fed1
Matt Jaffee <matthew.jaffee@gmail.com>
2018-05-15 16:08:22 -0500

    add godoc comment to enterprise/enterprise.go

--- enterprise/enterprise.go
+++ enterprise/enterprise.go
@@ -15,6 +15,10 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with Pilosa Enterprise Edition.  If not, see <http://www.gnu.org/licenses/>.
 
+// Package enterprise injects enterprise implementations of various Pilosa
+// features when Pilosa is built with "ENTERPRISE=1 make install". These
+// features are dual-licensed separately from Pilosa community edition under the
+// AGPL and Pilosa's commercial license.
 package enterprise
 
 import (
