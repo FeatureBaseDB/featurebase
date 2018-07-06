51619e81a69d3b00fc2270076b3b1c6e5ef9cc61
Cody Soyland <cody@soyland.org>
2018-07-05 21:47:51 -0500

    Unexport IndexInfo.Options

--- index.go
+++ index.go
@@ -406,7 +406,7 @@ func (p indexSlice) Less(i, j int) bool { return p[i].Name() < p[j].Name() }
 // IndexInfo represents schema information for an index.
 type IndexInfo struct {
 	Name    string       `json:"name"`
-	Options IndexOptions `json:"options"`
+	options IndexOptions `json:"options"`
 	Fields  []*FieldInfo `json:"fields"`
 }
 
