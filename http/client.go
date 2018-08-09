1d99da1f2871e66c69a122bd46dd30037fec7f3f
Todd Gruben <todd@pilosa.com>
2018-08-09 11:17:23 -0500

    added support for both pilosa and standard roaring uploads

--- http/client.go
+++ http/client.go
@@ -557,6 +557,7 @@ func (c *InternalClient) ImportRoaringBytes(ctx context.Context, node *pilosa.No
 	if resp.StatusCode != http.StatusOK {
 		return fmt.Errorf("invalid status: %d", resp.StatusCode)
 	}
+	io.Copy(ioutil.Discard, resp.Body)
 
 	return nil
 }
