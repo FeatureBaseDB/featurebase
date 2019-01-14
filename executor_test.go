600b39e4e575e76bc18224f8ba4cac9480be3459
Yuce Tekol <yucetekol@gmail.com>
2019-01-14 23:40:31 +0300

    updates row range test with a timestamp >  the default end timestamp

--- executor_test.go
+++ executor_test.go
@@ -1480,17 +1480,21 @@ func TestExecutor_Execute_Sum(t *testing.T) {
 // Ensure a range query can be executed.
 func TestExecutor_Execute_Row_Range(t *testing.T) {
 	t.Run("RowIDColumnID", func(t *testing.T) {
-		writeQuery := `
+		// Create a timestamp just out of the current date + 1 day timestamp (default end timestamp).
+		nextDayExclusive := time.Now().AddDate(0, 0, 1).Add(1 * time.Second)
+
+		writeQuery := fmt.Sprintf(`
 		Set(2, f=1, 1999-12-31T00:00)
 		Set(3, f=1, 2000-01-01T00:00)
 		Set(4, f=1, 2000-01-02T00:00)
 		Set(5, f=1, 2000-02-01T00:00)
 		Set(6, f=1, 2001-01-01T00:00)
 		Set(7, f=1, 2002-01-01T02:00)
+		Set(8, f=1, %s)
 
 		Set(2, f=1, 1999-12-30T00:00)
 		Set(2, f=1, 2002-02-01T00:00)
-		Set(2, f=10, 2001-01-01T00:00)`
+		Set(2, f=10, 2001-01-01T00:00)`, nextDayExclusive.Format("2006-01-02T15:04"))
 		readQueries := []string{
 			`Row(f=1, from=1999-12-31T00:00, to=2002-01-01T03:00)`,
 			`Row(f=1, from=1999-12-31T00:00)`,
