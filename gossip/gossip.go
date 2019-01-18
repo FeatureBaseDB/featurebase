6f4dee5e31f30547d18b1906589d327a08e813f7
Matt Jaffee <matthew.jaffee@gmail.com>
2019-01-18 14:06:10 -0600

    pass loggers around properly in gossip

--- gossip/gossip.go
+++ gossip/gossip.go
@@ -22,6 +22,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net"
+	"os"
 	"strconv"
 	"strings"
 	"sync"
@@ -151,7 +152,11 @@ func WithTransport(transport *Transport) memberSetOption {
 	}
 }
 
-// WithLogger is a functional option for providing a logger to NewMemberSet.
+// WithLogger is a functional option for providing a Go logger to NewMemberSet.
+// If the memberSet's transport is nil, this logger will be used when creating
+// one. If WithLogOutput is not used, this logger will be passed to memberlist
+// for it to use internally. This logger is not used for logging by code in this
+// (gossip) package - for that, use the WithPilosaLogger option.
 func WithLogger(logger *log.Logger) memberSetOption {
 	return func(g *memberSet) error {
 		g.logger = logger
@@ -159,6 +164,8 @@ func WithLogger(logger *log.Logger) memberSetOption {
 	}
 }
 
+// WithLogOutput allows one to pass a Writer which will in turn be passed to
+// memberlist for use in logging.
 func WithLogOutput(o io.Writer) memberSetOption {
 	return func(g *memberSet) error {
 		g.logOutput = o
@@ -166,7 +173,20 @@ func WithLogOutput(o io.Writer) memberSetOption {
 	}
 }
 
-// NewMemberSet returns a new instance of GossipMemberSet based on options.
+// WithPilosaLogger allows one to configure a memberSet with a logger of their
+// choice which satisfies the pilosa logger interface.
+func WithPilosaLogger(l logger.Logger) memberSetOption {
+	return func(g *memberSet) error {
+		g.Logger = l
+		return nil
+	}
+}
+
+// NewMemberSet returns a new instance of GossipMemberSet based on options. The
+// logging options which can be passed to NewMemberSet are complicated for
+// historical reasons - please pass WithPilosaLogger, and either WithLogOutput
+// or WithLogger. If you pass WithLogOutput, be sure to also pass in a Transport
+// using WithTransport.
 func NewMemberSet(cfg Config, api *pilosa.API, options ...memberSetOption) (*memberSet, error) {
 	host := api.Node().URI.Host
 	g := &memberSet{
@@ -180,13 +200,22 @@ func NewMemberSet(cfg Config, api *pilosa.API, options ...memberSetOption) (*mem
 			return nil, errors.Wrap(err, "executing option")
 		}
 	}
-	ger := newEventReceiver(g.logger, api)
+
+	ger := newEventReceiver(g.Logger, api)
 	g.eventReceiver = ger
 
 	if g.transport == nil {
 		port, err := strconv.Atoi(cfg.Port)
 		if err != nil {
 			return nil, fmt.Errorf("convert port: %s", err)
+		}
+
+		if g.logger == nil {
+			if g.logOutput != nil {
+				g.logger = logger.NewStandardLogger(g.logOutput).Logger()
+			} else {
+				g.logger = log.New(os.Stderr, "", log.LstdFlags)
+			}
 		}
 
 		// Set up the transport.
@@ -318,11 +347,11 @@ type eventReceiver struct {
 	ch   chan memberlist.NodeEvent
 	papi *pilosa.API
 
-	logger *log.Logger
+	logger logger.Logger
 }
 
 // newEventReceiver returns a new instance of GossipEventReceiver.
-func newEventReceiver(logger *log.Logger, papi *pilosa.API) *eventReceiver {
+func newEventReceiver(logger logger.Logger, papi *pilosa.API) *eventReceiver {
 	ger := &eventReceiver{
 		ch:     make(chan memberlist.NodeEvent, 1),
 		logger: logger,
