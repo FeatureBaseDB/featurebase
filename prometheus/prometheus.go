04b5d4b79be2d493daaf3fc22ea31a8a17324d90
Cody Soyland <cody@soyland.org>
2019-06-07 09:00:20 -0500

    Use seconds, not milliseconds for timings

--- prometheus/prometheus.go
+++ prometheus/prometheus.go
@@ -233,7 +233,7 @@ func (c *prometheusClient) Set(name string, value string, rate float64) {
 
 // Timing tracks timing information for a metric.
 func (c *prometheusClient) Timing(name string, value time.Duration, rate float64) {
-	durationMs := value / time.Millisecond
+	durationMs := value / time.Second
 	c.Histogram(name, float64(durationMs), rate)
 }
 
