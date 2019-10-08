55f3d61103bbeccdafcb6b3b683f8bdf77e9d494
Todd Gruben <todd@pilosa.com>
2019-10-08 11:14:50 -0500

    inspect grpc endpoint proto

--- proto/grpc.pb.go
+++ proto/grpc.pb.go
@@ -369,37 +369,319 @@ func _ColumnResponse_OneofSizer(msg proto.Message) (n int) {
 	return n
 }
 
+type InspectRequest struct {
+	Index                string    `protobuf:"bytes,1,opt,name=index,proto3" json:"index,omitempty"`
+	Columns              []*Column `protobuf:"bytes,2,rep,name=columns,proto3" json:"columns,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}  `json:"-"`
+	XXX_unrecognized     []byte    `json:"-"`
+	XXX_sizecache        int32     `json:"-"`
+}
+
+func (m *InspectRequest) Reset()         { *m = InspectRequest{} }
+func (m *InspectRequest) String() string { return proto.CompactTextString(m) }
+func (*InspectRequest) ProtoMessage()    {}
+func (*InspectRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_bedfbfc9b54e5600, []int{4}
+}
+
+func (m *InspectRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_InspectRequest.Unmarshal(m, b)
+}
+func (m *InspectRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_InspectRequest.Marshal(b, m, deterministic)
+}
+func (m *InspectRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_InspectRequest.Merge(m, src)
+}
+func (m *InspectRequest) XXX_Size() int {
+	return xxx_messageInfo_InspectRequest.Size(m)
+}
+func (m *InspectRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_InspectRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_InspectRequest proto.InternalMessageInfo
+
+func (m *InspectRequest) GetIndex() string {
+	if m != nil {
+		return m.Index
+	}
+	return ""
+}
+
+func (m *InspectRequest) GetColumns() []*Column {
+	if m != nil {
+		return m.Columns
+	}
+	return nil
+}
+
+type Column struct {
+	// Types that are valid to be assigned to ColumnType:
+	//	*Column_Id
+	//	*Column_Key
+	ColumnType           isColumn_ColumnType `protobuf_oneof:"columnType"`
+	XXX_NoUnkeyedLiteral struct{}            `json:"-"`
+	XXX_unrecognized     []byte              `json:"-"`
+	XXX_sizecache        int32               `json:"-"`
+}
+
+func (m *Column) Reset()         { *m = Column{} }
+func (m *Column) String() string { return proto.CompactTextString(m) }
+func (*Column) ProtoMessage()    {}
+func (*Column) Descriptor() ([]byte, []int) {
+	return fileDescriptor_bedfbfc9b54e5600, []int{5}
+}
+
+func (m *Column) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_Column.Unmarshal(m, b)
+}
+func (m *Column) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_Column.Marshal(b, m, deterministic)
+}
+func (m *Column) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_Column.Merge(m, src)
+}
+func (m *Column) XXX_Size() int {
+	return xxx_messageInfo_Column.Size(m)
+}
+func (m *Column) XXX_DiscardUnknown() {
+	xxx_messageInfo_Column.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_Column proto.InternalMessageInfo
+
+type isColumn_ColumnType interface {
+	isColumn_ColumnType()
+}
+
+type Column_Id struct {
+	Id int64 `protobuf:"varint,1,opt,name=id,proto3,oneof"`
+}
+
+type Column_Key struct {
+	Key string `protobuf:"bytes,2,opt,name=key,proto3,oneof"`
+}
+
+func (*Column_Id) isColumn_ColumnType() {}
+
+func (*Column_Key) isColumn_ColumnType() {}
+
+func (m *Column) GetColumnType() isColumn_ColumnType {
+	if m != nil {
+		return m.ColumnType
+	}
+	return nil
+}
+
+func (m *Column) GetId() int64 {
+	if x, ok := m.GetColumnType().(*Column_Id); ok {
+		return x.Id
+	}
+	return 0
+}
+
+func (m *Column) GetKey() string {
+	if x, ok := m.GetColumnType().(*Column_Key); ok {
+		return x.Key
+	}
+	return ""
+}
+
+// XXX_OneofFuncs is for the internal use of the proto package.
+func (*Column) XXX_OneofFuncs() (func(msg proto.Message, b *proto.Buffer) error, func(msg proto.Message, tag, wire int, b *proto.Buffer) (bool, error), func(msg proto.Message) (n int), []interface{}) {
+	return _Column_OneofMarshaler, _Column_OneofUnmarshaler, _Column_OneofSizer, []interface{}{
+		(*Column_Id)(nil),
+		(*Column_Key)(nil),
+	}
+}
+
+func _Column_OneofMarshaler(msg proto.Message, b *proto.Buffer) error {
+	m := msg.(*Column)
+	// columnType
+	switch x := m.ColumnType.(type) {
+	case *Column_Id:
+		b.EncodeVarint(1<<3 | proto.WireVarint)
+		b.EncodeVarint(uint64(x.Id))
+	case *Column_Key:
+		b.EncodeVarint(2<<3 | proto.WireBytes)
+		b.EncodeStringBytes(x.Key)
+	case nil:
+	default:
+		return fmt.Errorf("Column.ColumnType has unexpected type %T", x)
+	}
+	return nil
+}
+
+func _Column_OneofUnmarshaler(msg proto.Message, tag, wire int, b *proto.Buffer) (bool, error) {
+	m := msg.(*Column)
+	switch tag {
+	case 1: // columnType.id
+		if wire != proto.WireVarint {
+			return true, proto.ErrInternalBadWireType
+		}
+		x, err := b.DecodeVarint()
+		m.ColumnType = &Column_Id{int64(x)}
+		return true, err
+	case 2: // columnType.key
+		if wire != proto.WireBytes {
+			return true, proto.ErrInternalBadWireType
+		}
+		x, err := b.DecodeStringBytes()
+		m.ColumnType = &Column_Key{x}
+		return true, err
+	default:
+		return false, nil
+	}
+}
+
+func _Column_OneofSizer(msg proto.Message) (n int) {
+	m := msg.(*Column)
+	// columnType
+	switch x := m.ColumnType.(type) {
+	case *Column_Id:
+		n += 1 // tag and wire
+		n += proto.SizeVarint(uint64(x.Id))
+	case *Column_Key:
+		n += 1 // tag and wire
+		n += proto.SizeVarint(uint64(len(x.Key)))
+		n += len(x.Key)
+	case nil:
+	default:
+		panic(fmt.Sprintf("proto: unexpected type %T in oneof", x))
+	}
+	return n
+}
+
+type InspectResponse struct {
+	Set                  []*FieldSet `protobuf:"bytes,1,rep,name=set,proto3" json:"set,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}    `json:"-"`
+	XXX_unrecognized     []byte      `json:"-"`
+	XXX_sizecache        int32       `json:"-"`
+}
+
+func (m *InspectResponse) Reset()         { *m = InspectResponse{} }
+func (m *InspectResponse) String() string { return proto.CompactTextString(m) }
+func (*InspectResponse) ProtoMessage()    {}
+func (*InspectResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_bedfbfc9b54e5600, []int{6}
+}
+
+func (m *InspectResponse) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_InspectResponse.Unmarshal(m, b)
+}
+func (m *InspectResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_InspectResponse.Marshal(b, m, deterministic)
+}
+func (m *InspectResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_InspectResponse.Merge(m, src)
+}
+func (m *InspectResponse) XXX_Size() int {
+	return xxx_messageInfo_InspectResponse.Size(m)
+}
+func (m *InspectResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_InspectResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_InspectResponse proto.InternalMessageInfo
+
+func (m *InspectResponse) GetSet() []*FieldSet {
+	if m != nil {
+		return m.Set
+	}
+	return nil
+}
+
+type FieldSet struct {
+	FieldName            string    `protobuf:"bytes,1,opt,name=fieldName,proto3" json:"fieldName,omitempty"`
+	Columns              []*Column `protobuf:"bytes,2,rep,name=columns,proto3" json:"columns,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}  `json:"-"`
+	XXX_unrecognized     []byte    `json:"-"`
+	XXX_sizecache        int32     `json:"-"`
+}
+
+func (m *FieldSet) Reset()         { *m = FieldSet{} }
+func (m *FieldSet) String() string { return proto.CompactTextString(m) }
+func (*FieldSet) ProtoMessage()    {}
+func (*FieldSet) Descriptor() ([]byte, []int) {
+	return fileDescriptor_bedfbfc9b54e5600, []int{7}
+}
+
+func (m *FieldSet) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_FieldSet.Unmarshal(m, b)
+}
+func (m *FieldSet) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_FieldSet.Marshal(b, m, deterministic)
+}
+func (m *FieldSet) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_FieldSet.Merge(m, src)
+}
+func (m *FieldSet) XXX_Size() int {
+	return xxx_messageInfo_FieldSet.Size(m)
+}
+func (m *FieldSet) XXX_DiscardUnknown() {
+	xxx_messageInfo_FieldSet.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_FieldSet proto.InternalMessageInfo
+
+func (m *FieldSet) GetFieldName() string {
+	if m != nil {
+		return m.FieldName
+	}
+	return ""
+}
+
+func (m *FieldSet) GetColumns() []*Column {
+	if m != nil {
+		return m.Columns
+	}
+	return nil
+}
+
 func init() {
 	proto.RegisterType((*QueryPQLRequest)(nil), "QueryPQLRequest")
 	proto.RegisterType((*RowResponse)(nil), "RowResponse")
 	proto.RegisterType((*ColumnInfo)(nil), "ColumnInfo")
 	proto.RegisterType((*ColumnResponse)(nil), "ColumnResponse")
+	proto.RegisterType((*InspectRequest)(nil), "InspectRequest")
+	proto.RegisterType((*Column)(nil), "Column")
+	proto.RegisterType((*InspectResponse)(nil), "InspectResponse")
+	proto.RegisterType((*FieldSet)(nil), "FieldSet")
 }
 
 func init() { proto.RegisterFile("grpc.proto", fileDescriptor_bedfbfc9b54e5600) }
 
 var fileDescriptor_bedfbfc9b54e5600 = []byte{
-	// 316 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x5c, 0x91, 0x4f, 0x6b, 0xc2, 0x40,
-	0x10, 0xc5, 0xb3, 0xc6, 0xfa, 0x67, 0x22, 0x55, 0x86, 0x1e, 0x82, 0x87, 0x22, 0x39, 0x59, 0x28,
-	0xa1, 0xd8, 0x4b, 0x0b, 0x3d, 0xb5, 0x17, 0x0b, 0x2d, 0xe8, 0x1e, 0xbc, 0x96, 0x55, 0x57, 0x09,
-	0xac, 0x3b, 0x71, 0x37, 0xa1, 0xf5, 0x2b, 0xf4, 0xd2, 0xaf, 0x5c, 0xb2, 0x49, 0xb4, 0xf6, 0x36,
-	0xef, 0xfd, 0x86, 0xf9, 0xc3, 0x03, 0xd8, 0x9a, 0x74, 0x15, 0xa7, 0x86, 0x32, 0x8a, 0x1e, 0xa1,
-	0x3f, 0xcf, 0xa5, 0x39, 0xcc, 0xe6, 0x6f, 0x5c, 0xee, 0x73, 0x69, 0x33, 0xbc, 0x82, 0x8b, 0x44,
-	0xaf, 0xe5, 0x57, 0xc8, 0x46, 0x6c, 0xdc, 0xe5, 0xa5, 0xc0, 0x01, 0xf8, 0xe9, 0x5e, 0x85, 0x0d,
-	0xe7, 0x15, 0x65, 0xf4, 0xcd, 0x20, 0xe0, 0xf4, 0xc9, 0xa5, 0x4d, 0x49, 0x5b, 0x89, 0xb7, 0x10,
-	0xac, 0x48, 0xe5, 0x3b, 0xfd, 0x91, 0xe8, 0x0d, 0x85, 0x6c, 0xe4, 0x8f, 0x83, 0x49, 0x10, 0xbf,
-	0x38, 0xef, 0x55, 0x6f, 0x88, 0xc3, 0xea, 0x58, 0xe3, 0x0d, 0xb4, 0x4b, 0x65, 0xc3, 0x86, 0xeb,
-	0xec, 0x57, 0x9d, 0xf5, 0x3c, 0x5e, 0x73, 0x8c, 0xa0, 0x27, 0x8d, 0x21, 0xf3, 0x2e, 0xad, 0x15,
-	0x5b, 0x19, 0xfa, 0xee, 0x86, 0x33, 0x2f, 0x7a, 0x02, 0x38, 0x2d, 0x42, 0x84, 0xa6, 0x16, 0x3b,
-	0x59, 0x7d, 0xe0, 0x6a, 0x1c, 0x42, 0x67, 0x2d, 0x32, 0x91, 0x1d, 0x52, 0x59, 0x7d, 0x71, 0xd4,
-	0xd1, 0x0f, 0x83, 0xcb, 0xf3, 0xed, 0x78, 0x0d, 0x5d, 0x9b, 0x99, 0x44, 0x6f, 0x17, 0x42, 0x95,
-	0x73, 0xa6, 0x1e, 0x3f, 0x59, 0x18, 0x42, 0x2b, 0xd1, 0x59, 0x01, 0x8b, 0x61, 0xfe, 0xd4, 0xe3,
-	0x95, 0xc6, 0x21, 0xb4, 0x97, 0x44, 0xaa, 0x40, 0xc5, 0xa5, 0x9d, 0xa9, 0xc7, 0x6b, 0xc3, 0x31,
-	0x45, 0xcb, 0x82, 0x35, 0x47, 0x6c, 0xdc, 0x73, 0xac, 0x34, 0x9e, 0x03, 0xe8, 0x96, 0x1f, 0x2f,
-	0x84, 0x9a, 0x3c, 0x40, 0x6b, 0x96, 0x28, 0xb2, 0x02, 0x63, 0xe8, 0xd4, 0x09, 0xe1, 0x20, 0xfe,
-	0x17, 0xd6, 0xb0, 0x17, 0xff, 0x89, 0x20, 0xf2, 0xee, 0xd8, 0xb2, 0xe5, 0x82, 0xbd, 0xff, 0x0d,
-	0x00, 0x00, 0xff, 0xff, 0xd1, 0x44, 0x4d, 0x1b, 0xe6, 0x01, 0x00, 0x00,
+	// 433 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x8c, 0x53, 0x4d, 0x8f, 0xd3, 0x30,
+	0x10, 0x4d, 0x9a, 0xa5, 0x6d, 0x26, 0xd5, 0xb6, 0x1a, 0x71, 0x88, 0x0a, 0x42, 0xc5, 0xa7, 0x22,
+	0x21, 0x6b, 0xb5, 0x9c, 0x90, 0xf6, 0xb4, 0x48, 0xa8, 0x15, 0x1f, 0xda, 0x35, 0x68, 0xaf, 0x28,
+	0x6d, 0xa6, 0x55, 0x20, 0x6b, 0x67, 0x63, 0x57, 0xd0, 0xbf, 0xc0, 0x85, 0xbf, 0x8c, 0xec, 0x38,
+	0x0d, 0xed, 0x01, 0xed, 0x6d, 0xe6, 0x3d, 0x7b, 0xfc, 0xe6, 0xcd, 0x18, 0x60, 0x5b, 0x57, 0x6b,
+	0x5e, 0xd5, 0xca, 0x28, 0xf6, 0x16, 0xc6, 0xb7, 0x3b, 0xaa, 0xf7, 0x37, 0xb7, 0x1f, 0x05, 0x3d,
+	0xec, 0x48, 0x1b, 0x7c, 0x0a, 0x4f, 0x0a, 0x99, 0xd3, 0xaf, 0x34, 0x9c, 0x85, 0xf3, 0x58, 0x34,
+	0x09, 0x4e, 0x20, 0xaa, 0x1e, 0xca, 0xb4, 0xe7, 0x30, 0x1b, 0xb2, 0xdf, 0x21, 0x24, 0x42, 0xfd,
+	0x14, 0xa4, 0x2b, 0x25, 0x35, 0xe1, 0x6b, 0x48, 0xd6, 0xaa, 0xdc, 0xdd, 0xcb, 0x6f, 0x85, 0xdc,
+	0xa8, 0x34, 0x9c, 0x45, 0xf3, 0xe4, 0x32, 0xe1, 0xef, 0x1c, 0xb6, 0x94, 0x1b, 0x25, 0x60, 0x7d,
+	0x88, 0xf1, 0x15, 0x0c, 0x9a, 0x4c, 0xa7, 0x3d, 0x77, 0x72, 0xec, 0x4f, 0xb6, 0xf5, 0x44, 0xcb,
+	0x23, 0x83, 0x11, 0xd5, 0xb5, 0xaa, 0x3f, 0x91, 0xd6, 0xd9, 0x96, 0xd2, 0xc8, 0x69, 0x38, 0xc2,
+	0xd8, 0x15, 0x40, 0xf7, 0x10, 0x22, 0x9c, 0xc9, 0xec, 0x9e, 0x7c, 0x07, 0x2e, 0xc6, 0x29, 0x0c,
+	0xf3, 0xcc, 0x64, 0x66, 0x5f, 0x91, 0xef, 0xe2, 0x90, 0xb3, 0x3f, 0x21, 0x9c, 0x1f, 0xbf, 0x8e,
+	0x2f, 0x20, 0xd6, 0xa6, 0x2e, 0xe4, 0xf6, 0x2e, 0x2b, 0x9b, 0x3a, 0x8b, 0x40, 0x74, 0x10, 0xa6,
+	0xd0, 0x2f, 0xa4, 0xb1, 0xa4, 0x2d, 0x16, 0x2d, 0x02, 0xe1, 0x73, 0x9c, 0xc2, 0x60, 0xa5, 0x54,
+	0x69, 0x29, 0xab, 0x74, 0xb8, 0x08, 0x44, 0x0b, 0x38, 0xae, 0x54, 0x2b, 0xcb, 0x9d, 0xcd, 0xc2,
+	0xf9, 0xc8, 0x71, 0x0d, 0x70, 0x9d, 0x40, 0xdc, 0x74, 0x7c, 0x97, 0x95, 0x6c, 0x09, 0xe7, 0x4b,
+	0xa9, 0x2b, 0x5a, 0x9b, 0xff, 0x8f, 0xe5, 0xe5, 0xa9, 0x8d, 0x83, 0xd6, 0xc6, 0x16, 0x67, 0x57,
+	0xd0, 0x6f, 0x20, 0x9c, 0x40, 0xaf, 0xc8, 0xdd, 0x7d, 0xab, 0xb7, 0x57, 0xe4, 0x88, 0x10, 0xfd,
+	0xa0, 0x7d, 0xe3, 0xc7, 0x22, 0x10, 0x36, 0xb9, 0x1e, 0x81, 0x9f, 0xd3, 0x57, 0x6b, 0x0d, 0x87,
+	0xf1, 0x41, 0x88, 0xb7, 0xe6, 0x19, 0x44, 0x9a, 0x8c, 0x1f, 0x70, 0xcc, 0xdf, 0x17, 0x54, 0xe6,
+	0x5f, 0xc8, 0x08, 0x8b, 0xb2, 0x0f, 0x30, 0x6c, 0x01, 0x7c, 0x0e, 0xf1, 0xc6, 0xc6, 0x9f, 0xbb,
+	0x59, 0x74, 0xc0, 0x23, 0xa4, 0x5f, 0x7e, 0x87, 0xfe, 0x4d, 0x51, 0x2a, 0x9d, 0x21, 0x87, 0x61,
+	0xbb, 0xa7, 0x38, 0xe1, 0x27, 0x2b, 0x3b, 0x1d, 0xf1, 0x7f, 0x16, 0x91, 0x05, 0x17, 0x21, 0x5e,
+	0xc0, 0xc0, 0xcb, 0xc6, 0x31, 0x3f, 0x76, 0x72, 0x3a, 0xe1, 0x27, 0x1d, 0xd9, 0x1b, 0xab, 0xbe,
+	0xfb, 0x10, 0x6f, 0xfe, 0x06, 0x00, 0x00, 0xff, 0xff, 0xec, 0x56, 0xc3, 0x58, 0x1e, 0x03, 0x00,
+	0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -415,6 +697,7 @@ const _ = grpc.SupportPackageIsVersion4
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type PilosaClient interface {
 	QueryPQL(ctx context.Context, in *QueryPQLRequest, opts ...grpc.CallOption) (Pilosa_QueryPQLClient, error)
+	Inspect(ctx context.Context, in *InspectRequest, opts ...grpc.CallOption) (Pilosa_InspectClient, error)
 }
 
 type pilosaClient struct {
@@ -457,9 +740,42 @@ func (x *pilosaQueryPQLClient) Recv() (*RowResponse, error) {
 	return m, nil
 }
 
+func (c *pilosaClient) Inspect(ctx context.Context, in *InspectRequest, opts ...grpc.CallOption) (Pilosa_InspectClient, error) {
+	stream, err := c.cc.NewStream(ctx, &_Pilosa_serviceDesc.Streams[1], "/Pilosa/Inspect", opts...)
+	if err != nil {
+		return nil, err
+	}
+	x := &pilosaInspectClient{stream}
+	if err := x.ClientStream.SendMsg(in); err != nil {
+		return nil, err
+	}
+	if err := x.ClientStream.CloseSend(); err != nil {
+		return nil, err
+	}
+	return x, nil
+}
+
+type Pilosa_InspectClient interface {
+	Recv() (*InspectResponse, error)
+	grpc.ClientStream
+}
+
+type pilosaInspectClient struct {
+	grpc.ClientStream
+}
+
+func (x *pilosaInspectClient) Recv() (*InspectResponse, error) {
+	m := new(InspectResponse)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
 // PilosaServer is the server API for Pilosa service.
 type PilosaServer interface {
 	QueryPQL(*QueryPQLRequest, Pilosa_QueryPQLServer) error
+	Inspect(*InspectRequest, Pilosa_InspectServer) error
 }
 
 func RegisterPilosaServer(s *grpc.Server, srv PilosaServer) {
@@ -487,6 +803,27 @@ func (x *pilosaQueryPQLServer) Send(m *RowResponse) error {
 	return x.ServerStream.SendMsg(m)
 }
 
+func _Pilosa_Inspect_Handler(srv interface{}, stream grpc.ServerStream) error {
+	m := new(InspectRequest)
+	if err := stream.RecvMsg(m); err != nil {
+		return err
+	}
+	return srv.(PilosaServer).Inspect(m, &pilosaInspectServer{stream})
+}
+
+type Pilosa_InspectServer interface {
+	Send(*InspectResponse) error
+	grpc.ServerStream
+}
+
+type pilosaInspectServer struct {
+	grpc.ServerStream
+}
+
+func (x *pilosaInspectServer) Send(m *InspectResponse) error {
+	return x.ServerStream.SendMsg(m)
+}
+
 var _Pilosa_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "Pilosa",
 	HandlerType: (*PilosaServer)(nil),
@@ -495,6 +832,11 @@ var _Pilosa_serviceDesc = grpc.ServiceDesc{
 		{
 			StreamName:    "QueryPQL",
 			Handler:       _Pilosa_QueryPQL_Handler,
+			ServerStreams: true,
+		},
+		{
+			StreamName:    "Inspect",
+			Handler:       _Pilosa_Inspect_Handler,
 			ServerStreams: true,
 		},
 	},
