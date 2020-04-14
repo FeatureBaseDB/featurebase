08fa90e0f3dddcb88fbe43c95f861214cbe891bd
Cody Soyland <cody@soyland.org>
2020-04-14 16:11:34 -0500

    Reduce duplication of setupLogger arch-specific code

---
+++ server/dup.go
@@ -0,0 +1,26 @@
+// Copyright 2017 Pilosa Corp.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build !arm64
+
+package server
+
+import (
+	"syscall"
+)
+
+// dup is an alias for syscall.Dup2 on most platforms or syscall.Dup3 on ARM64
+func (m *Command) dup(oldfd int, newfd int) error {
+	return syscall.Dup2(oldfd, newfd)
+}
--- server/server.go
+++ server/server.go
@@ -414,6 +414,30 @@ func (m *Command) setupNetworking() error {
 	return errors.Wrap(gossipMemberSet.Open(), "opening gossip memberset")
 }
 
+// setupLogger sets up the logger based on the configuration.
+func (m *Command) setupLogger() error {
+	if m.Config.LogPath == "" {
+		m.logOutput = m.Stderr
+	} else {
+		f, err := os.OpenFile(m.Config.LogPath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0600)
+		if err != nil {
+			return errors.Wrap(err, "opening file")
+		}
+		m.logOutput = f
+		err = m.dup(int(f.Fd()), int(os.Stderr.Fd()))
+		if err != nil {
+			return errors.Wrap(err, "syscall dup stderr to logfile")
+		}
+	}
+
+	if m.Config.Verbose {
+		m.logger = logger.NewVerboseLogger(m.logOutput)
+	} else {
+		m.logger = logger.NewStandardLogger(m.logOutput)
+	}
+	return nil
+}
+
 // GossipTransport allows a caller to return the gossip transport created when
 // setting up the GossipMemberSet. This is useful if one needs to determine the
 // allocated ephemeral port programmatically. (usually used in tests)
