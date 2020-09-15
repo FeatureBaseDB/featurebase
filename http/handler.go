76715a3f9ddbb72d9c52ebbfd6ff6eaf5537dc1b
Kuba Podgórski <kuba--@users.noreply.github.com>
2020-09-15 19:09:59 +0200

    Fix TranslateStore writable

--- http/handler.go
+++ http/handler.go
@@ -2432,17 +2432,22 @@ func (h *Handler) handlePostTranslateKeys(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	buf, err := h.api.TranslateKeys(r.Context(), r.Body, true)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("translate keys: %v", err), http.StatusInternalServerError)
-		return
-	}
+	buf, err := h.api.TranslateKeys(r.Context(), r.Body)
+	switch errors.Cause(err) {
+	case nil:
+		// Write response.
+		if _, err = w.Write(buf); err != nil {
+			h.logger.Printf("writing translate keys response: %v", err)
+		}
 
-	// Write response.
-	_, err = w.Write(buf)
-	if err != nil {
-		h.logger.Printf("writing translate keys response: %v", err)
-		return
+	case pilosa.ErrTranslatingKeyNotFound:
+		http.Error(w, fmt.Sprintf("translate keys: %v", err), http.StatusNotFound)
+
+	case pilosa.ErrTranslateStoreReadOnly:
+		http.Error(w, fmt.Sprintf("translate keys: %v", err), http.StatusPreconditionFailed)
+
+	default:
+		http.Error(w, fmt.Sprintf("translate keys: %v", err), http.StatusInternalServerError)
 	}
 }
 
