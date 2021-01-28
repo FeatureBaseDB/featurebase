60b6eecc7a81ad7438c3de013ee7a117c1eb7dce
Ben Johnson <benbjohnson@yahoo.com>
2021-01-28 16:36:45 -0700

    Update keyed/unkeyed benchmarks

--- scripts/bench_write.sh
+++ scripts/bench_write.sh
@@ -21,19 +21,22 @@ SHA=$(git -C $PILOSA_SRC rev-parse HEAD)
 # Format current date.
 DATE=$(date '+%Y%m%d')
 
-WORKFLOW_PATH="${BASH_SOURCE%/*}/etc/gloat/gh.1m.yml"
-WORKFLOW_NAME="$(gloat workflow name $WORKFLOW_PATH)"
-TITLE="RBF vs Roaring, $WORKFLOW_NAME, $DATE ($SHA)"
+for FILENAME in gh.1m.yml gh.issues.keyed.yml gh.issues.unkeyed.yml 
+do
+	WORKFLOW_PATH="${BASH_SOURCE%/*}/etc/gloat/${FILENAME}"
+	WORKFLOW_NAME="$(gloat workflow name $WORKFLOW_PATH)"
+	TITLE="RBF vs Roaring, $WORKFLOW_NAME, $DATE ($SHA)"
 
-# Execute RBF/Roaring benchmark.
-RBF_PATH=gloat/data/1m/rbf/${DATE}.tar.gz
-TXSRC=rbf gloat run -v -o $RBF_PATH $WORKFLOW_PATH
+	# Execute RBF/Roaring benchmark.
+	RBF_PATH=gloat/data/1m/rbf/${DATE}.tar.gz
+	TXSRC=rbf gloat run -v -o $RBF_PATH $WORKFLOW_PATH
 
-ROARING_PATH=gloat/data/1m/roaring/${DATE}.tar.gz
-TXSRC=roaring gloat run -v -o $ROARING_PATH $WORKFLOW_PATH
+	ROARING_PATH=gloat/data/1m/roaring/${DATE}.tar.gz
+	TXSRC=roaring gloat run -v -o $ROARING_PATH $WORKFLOW_PATH
 
-# Generate graph from results.
-gloat graph -layout 2,5 -size 5120,820 -title "$TITLE" -name utime,stime,heap_alloc,heap_inuse,heap_objects,num_gc,rchar,wchar,syscr,syscw -series rbf,roaring -o /tmp/output.png $RBF_PATH $ROARING_PATH
+	# Generate graph from results.
+	gloat graph -layout 2,5 -size 5120,820 -title "$TITLE" -name utime,stime,heap_alloc,heap_inuse,heap_objects,num_gc,rchar,wchar,syscr,syscw -series rbf,roaring -o /tmp/output.png $RBF_PATH $ROARING_PATH
 
-# Post graph to Slack with SHA.
-curl -F file=@/tmp/output.png -F channels=C01HBFKRLGH -F "initial_comment=$TITLE" -H "Authorization: Bearer $SLACK_OAUTH_TOKEN" https://slack.com/api/files.upload
+	# Post graph to Slack with SHA.
+	curl -F file=@/tmp/output.png -F channels=C01HBFKRLGH -F "initial_comment=$TITLE" -H "Authorization: Bearer $SLACK_OAUTH_TOKEN" https://slack.com/api/files.upload
+done
