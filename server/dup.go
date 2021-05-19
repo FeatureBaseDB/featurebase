b6098f05e6e4f5ab726f6c21816eb7149e6d45c6
Mahesh Arumugam <mahesh@molecula.com>
2021-05-19 11:15:12 -0700

    fixing the build for linux,arm

--- server/dup.go
+++ server/dup.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build darwin linux,amd64
+// +build darwin linux,!arm64
 
 package server
 
