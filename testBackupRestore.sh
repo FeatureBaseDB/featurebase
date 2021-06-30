3f8047778cfd476815f91e6241fa94eb61126233
Todd Gruben <todd@pilosa.com>
2021-06-30 14:52:15 -0500

    CI test for backup restore

---
+++ testBackupRestore.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -eux
+
+declare STATUS="NORMAL"
+declare TIMEOUT=30
+sleep 4
+STATUS=$STATUS timeout -s TERM $TIMEOUT bash -c \
+    'while [[ ${STATUS_RECEIVED} != ${STATUS} ]];\
+    	do STATUS_RECEIVED=$(curl --connect-timeout 1 -s pilosa0:10101/status | jq  -r ".state") && \
+        echo "received status: $STATUS_RECEIVED" && \
+        sleep 1;\
+    done;'
+echo "NOW DO STUFF"
+datagen --source kitchensink_keyed -e 9999 --pilosa.index sink --pilosa.batch-size 10000 --pilosa.hosts pilosa0:10101
+before=$(/pilosa chksum --host pilosa0:10101)
+/pilosa backup -o backupdir --host pilosa0:10101
+curl -X DELETE -s pilosa0:10101/index/sink
+/pilosa restore -s backupdir --host pilosa0:10101
+after=$(/pilosa chksum --host pilosa0:10101)
+if [ "$before" = "$after" ]; then
+	echo "PASS"
+	exit 0
+else
+	echo "FAIL"
+	exit 1
+fi
