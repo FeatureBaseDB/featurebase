5d070b47bb76ca73c2d2c50ae6393d9ea6916b79
Samir Patel <48686912+54mir@users.noreply.github.com>
2021-09-29 15:57:31 -0500

    clean up

--- cmd/roaring-migrate/ctim_darwin.go
+++ cmd/roaring-migrate/ctim_darwin.go
@@ -4,11 +4,10 @@ package main
 
 import (
 	"syscall"
-	"time"
 )
 
 func CTimeNano(stat *syscall.Stat_t) int64 {
+	NANOS := int64(1e9) // number of nanosecs in 1 sec
 	ts := stat.Ctimespec
-	time.Unix(int64(ts.Sec), int64(ts.Nsec))
-	return int64(ts.Sec)*1e9 + int64(ts.Nsec)
+	return ts.Sec*NANOS + ts.Nsec
 }
