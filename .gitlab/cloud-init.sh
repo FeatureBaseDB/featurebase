ebde0142da01c1c4d205a7b363c2a782c638fd42
Souhaila Noor <souhaila.noor@molecula.com>
2021-11-08 09:14:47 -0600

    add stage to configure featurebase

--- .gitlab/cloud-init.sh
+++ .gitlab/cloud-init.sh
@@ -4,7 +4,6 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
 # Install packages 
 yum update -y
-# yum install golang -y # latest verion in ec2 is 1.15.14 
 yum install postgresql -y
 
 # Configure host system  
@@ -13,7 +12,8 @@ sysctl -w fs.file-max=262144
 sysctl -p
 echo 'cat /proc/sys/fs/file-max'
 
-# install go 1.16.9
+# yum install golang -y # latest verion in ec2 is 1.15.14 
+# install go 1.16.9 manually
 curl -O https://dl.google.com/go/go1.16.9.linux-amd64.tar.gz
 tar xvf go1.16.9.linux-amd64.tar.gz
 chown -R root:root ./go
