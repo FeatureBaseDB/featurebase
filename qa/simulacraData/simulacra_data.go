90ace6b970c3e5cc3d450530c00eeff6bd110365
kcrodgers24 <kcrodgers@live.com>
2021-11-18 13:47:23 -0800

    deletes duplicate file and adds license header

--- qa/simulacraData/simulacra_data.go
+++ qa/simulacraData/simulacra_data.go
@@ -1,3 +1,17 @@
+// Copyright 2017 Pilosa Corp.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // PURPOSE: Generate data for Samsung unique workflow/use case, as described in Jira Ticket FB-971
 // INPUT: none
 // OUTPUT: 6 csv files, containing approx 1 billion lines of data associated to 200 million unique records (approx 28BGB of data)
