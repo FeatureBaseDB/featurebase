184b93555093444cb3eeb40c306d166dd7aef9fb
Souhaila Noor <souhaila.noor@molecula.com>
2021-11-23 16:07:22 -0600

    added print statement

--- qa/scripts/deployNode.sh
+++ qa/scripts/deployNode.sh
@@ -14,7 +14,8 @@ function configure_env() {
     which ssh-agent || ( apt-get update -y && apt-get install openssh-client -y )    
     eval `ssh-agent -s`
     mkdir -p ~/.ssh
-    echo "${AWS_SSH_PRIVATE_KEY}" | ssh-add -
+    echo "$AWS_SSH_PRIVATE_KEY" 
+    echo "$AWS_SSH_PRIVATE_KEY" | ssh-add -
     chmod 700 /root/.ssh
 
     # install jq
