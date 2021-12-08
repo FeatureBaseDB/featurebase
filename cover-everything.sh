a40bc3edf7811bdd022269f37a7ed16318293e67
Matthew Jaffee <jaffee@pilosa.com>
2021-12-07 18:38:48 -0600

    try to simplify test coverage w/ -coverpkg

--- cover-everything.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# actually get test coverage for every single package and subpackage
-# very slow but oh well what are you gonna do, not test things?  
-# note it skips the roaring migrate
-echo "mode: atomic" > coverage.out
-for pkg in $(go list all | grep featurebase ); do
-    go test -coverprofile=pkgcoverage.out -covermode=atomic $pkg;
-    tail -n +2 pkgcoverage.out >> coverage.out; done
