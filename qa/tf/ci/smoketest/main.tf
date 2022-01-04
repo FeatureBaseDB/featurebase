ca5633c5946e30eee173f9b3d8a4d50c5361c789
pokeeffe-molecula <patrick.okeeffe@molecula.com>
2022-01-04 16:36:11 -0600

    add uniqueness to cluster prefix

--- qa/tf/ci/smoketest/main.tf
+++ qa/tf/ci/smoketest/main.tf
@@ -1,7 +1,7 @@
 
 module "ci-cluster" {
     source = "../../.modules/featurebase-cluster"
-    cluster_prefix = "smoke"
+    cluster_prefix = var.cluster_prefix
     region = var.region
     profile = var.profile
     fb_data_node_type = "m6g.large"
