bc89ca355364d99b448dcd43fa81f007a443b69f
pokeeffe-molecula <patrick.okeeffe@molecula.com>
2022-01-21 20:31:16 -0600

    now with real private_subnets!

--- qa/tf/ci/smoketest/main.tf
+++ qa/tf/ci/smoketest/main.tf
@@ -10,5 +10,5 @@ module "ci-cluster" {
     vpc_id = "vpc-05a26a122f961dc2b"
     vpc_cidr_block = "10.0.0.0/16"
     vpc_public_subnets = ["subnet-066b4b922b54e51a2","subnet-037b8884269a69025","subnet-08482631514426210",]
-    vpc_private_subnets = ["subnet-050b1219d78f2db1b","subnet-07155281789c6d33b","subnet-0d623c769e086e46e",]
+    vpc_private_subnets = ["subnet-0319dde319380326f","subnet-0517ca9a646d80f88","subnet-05a7b685ed27eb1cf",]
 }
