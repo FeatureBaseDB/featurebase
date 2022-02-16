08f5454f6bb5afcd1dc2e7bb68cd19a6f245b04b
pokeeffe-molecula <patrick.okeeffe@molecula.com>
2022-02-16 17:01:41 -0600

    run it from the shell script

--- qa/scripts/perf/able/ableTest.sh
+++ qa/scripts/perf/able/ableTest.sh
@@ -8,5 +8,29 @@ echo "using INGESTNODE0 ${INGESTNODE0}"
 DATANODE0=$(cat ./qa/tf/gauntlet/able/outputs.json | jq -r '[.data_node_ips][0]["value"][0]')
 echo "using DATANODE0 ${DATANODE0}"
 
+ssh -A -i ~/.ssh/gitlab-featurebase-ci.pem -o "StrictHostKeyChecking no" ec2-user@${INGESTNODE0} "sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C5AD17C747E3415A3642D57D77C6C491D6AC1D69"
+ssh -A -i ~/.ssh/gitlab-featurebase-ci.pem -o "StrictHostKeyChecking no" ec2-user@${INGESTNODE0} "echo 'deb https://dl.k6.io/deb stable main' | sudo tee /etc/apt/sources.list.d/k6.list"
+ssh -A -i ~/.ssh/gitlab-featurebase-ci.pem -o "StrictHostKeyChecking no" ec2-user@${INGESTNODE0} "sudo apt-get update"
+ssh -A -i ~/.ssh/gitlab-featurebase-ci.pem -o "StrictHostKeyChecking no" ec2-user@${INGESTNODE0} "sudo apt-get install k6"
 
-echo "Done."
+echo "Copying tests to remote"
+scp -r -i ~/.ssh/gitlab-featurebase-ci.pem ./qa/scripts/perf/able/*.js ec2-user@${INGESTNODE0}:/data
+if (( $? != 0 )) 
+then 
+    echo "Copy failed"
+    exit 1
+fi
+
+# run smoke test
+echo "Running smoke test..."
+ssh -A -i ~/.ssh/gitlab-featurebase-ci.pem -o "StrictHostKeyChecking no" ec2-user@${INGESTNODE0} "k6 run /data/script.js" 
+ABLETESTRESULT=$?
+
+if (( $ABLETESTRESULT != 0 )) 
+then 
+    echo "able perf test complete with failures"
+else
+    echo "able test complete"
+fi
+
+exit $ABLETESTRESULT 
