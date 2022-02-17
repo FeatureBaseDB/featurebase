4b407c10fc4c58ab537cf59110230f8d22482b8f
pokeeffe-molecula <patrick.okeeffe@molecula.com>
2022-02-17 14:29:07 -0600

    works on my machine

--- qa/scripts/perf/able/ableTest.sh
+++ qa/scripts/perf/able/ableTest.sh
@@ -9,8 +9,10 @@ DATANODE0=$(cat ./qa/tf/gauntlet/able/outputs.json | jq -r '[.data_node_ips][0][
 echo "using DATANODE0 ${DATANODE0}"
 
 
-ssh -A -i ~/.ssh/gitlab-featurebase-ci.pem -o "StrictHostKeyChecking no" ec2-user@${INGESTNODE0} "sudo dnf install https://dl.k6.io/rpm/repo.rpm"
-ssh -A -i ~/.ssh/gitlab-featurebase-ci.pem -o "StrictHostKeyChecking no" ec2-user@${INGESTNODE0} "sudo dnf install k6"
+ssh -A -i ~/.ssh/gitlab-featurebase-ci.pem -o "StrictHostKeyChecking no" ec2-user@${INGESTNODE0} "wget https://github.com/grafana/k6/releases/download/v0.36.0/k6-v0.36.0-linux-arm64.tar.gz"
+ssh -A -i ~/.ssh/gitlab-featurebase-ci.pem -o "StrictHostKeyChecking no" ec2-user@${INGESTNODE0} "tar -xvf k6-v0.36.0-linux-arm64.tar.gz"
+ssh -A -i ~/.ssh/gitlab-featurebase-ci.pem -o "StrictHostKeyChecking no" ec2-user@${INGESTNODE0} "mkdir bin"
+ssh -A -i ~/.ssh/gitlab-featurebase-ci.pem -o "StrictHostKeyChecking no" ec2-user@${INGESTNODE0} "mv ./k6-v0.36.0-linux-arm64/k6 ./bin"
 
 echo "Copying tests to remote"
 scp -r -i ~/.ssh/gitlab-featurebase-ci.pem ./qa/scripts/perf/able/*.js ec2-user@${INGESTNODE0}:/data
@@ -22,7 +24,7 @@ fi
 
 # run smoke test
 echo "Running smoke test..."
-ssh -A -i ~/.ssh/gitlab-featurebase-ci.pem -o "StrictHostKeyChecking no" ec2-user@${INGESTNODE0} "k6 run /data/script.js" 
+ssh -A -i ~/.ssh/gitlab-featurebase-ci.pem -o "StrictHostKeyChecking no" ec2-user@${INGESTNODE0} "/home/ec2-user/bin/k6 run -e DATANODE0=test.k6.io /data/script.js" 
 ABLETESTRESULT=$?
 
 if (( $ABLETESTRESULT != 0 )) 
@@ -31,5 +33,6 @@ then
 else
     echo "able test complete"
 fi
+
 
 exit $ABLETESTRESULT 
--- qa/scripts/perf/able/script.js
+++ qa/scripts/perf/able/script.js
@@ -2,6 +2,6 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 	
 export default function () {
-    http.get('https://test.k6.io');
+    http.get('https://${__ENV.MY_HOSTNAME}');
     sleep(1);
 }
