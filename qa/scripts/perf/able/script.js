ae0c70d60b8330559ad15b085944b20042f81b01
pokeeffe-molecula <patrick.okeeffe@molecula.com>
2022-02-17 18:20:05 -0600

    interpolate all the js strings

--- qa/scripts/perf/able/script.js
+++ qa/scripts/perf/able/script.js
@@ -2,6 +2,6 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 	
 export default function () {
-    http.get('https://${__ENV.MY_HOSTNAME}');
+    http.get(`https://${__ENV.DATANODE0}`);
     sleep(1);
 }
