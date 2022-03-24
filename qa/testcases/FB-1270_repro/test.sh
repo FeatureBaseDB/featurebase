bb637131b9d81e8c8715c8b9a6c5b703a267abac
Matthew Jaffee <jaffee@pilosa.com>
2022-03-24 13:33:11 -0500

    delete query was syntactically broken and unecessary
    the issue reproduced even though this query was broken, so it can be removed.

--- qa/testcases/FB-1270_repro/test.sh
+++ qa/testcases/FB-1270_repro/test.sh
@@ -7,5 +7,4 @@ for i in {41..44}; do
     echo "ROUND $i"
     echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
     /data/datagen_linux_arm64 -s texas_health --pilosa.index thr --end-at 1048575 --pilosa.batch-size 1048576 --concurrency 1 --seed=$i --pilosa.hosts=$1
-    curl -XPOST $1/index/thr/query -d 'Delete(Row(timestamp<2020-12-13T00:32:42Z)'
 done
