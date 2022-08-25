650b7eaa45502af790be00c2f0cd0953563aeedc
Hoang Pham <hoang.pham@molecula.com>
2022-08-25 18:50:35 -0500

    FB-1627 - Added backup/restore tar. Purpose: for cloud team to backup and restore directly through stdout, stdin

---
+++ cmd/restore_tar.go
@@ -0,0 +1,40 @@
+// Copyright 2022 Molecula Corp. All rights reserved.
+package cmd
+
+import (
+	"context"
+	"io"
+
+	"github.com/molecula/featurebase/v3/ctl"
+	"github.com/spf13/cobra"
+)
+
+func newRestoreTarCommand(stdin io.Reader, stdout, stderr io.Writer) *cobra.Command {
+	cmd := ctl.NewRestoreTarCommand(stdin, stdout, stderr)
+	restoreCmd := &cobra.Command{
+		Use:   "restoretar",
+		Short: "Restore from a backup in tar format",
+		Long: `
+The Restore command will take a tar-formatted backup archive and restore it to a new, clean cluster.
+`,
+		RunE: func(c *cobra.Command, args []string) error {
+			return cmd.Run(context.Background())
+		},
+	}
+	flags := restoreCmd.Flags()
+	flags.StringVarP(&cmd.Path, "source", "s", "", "backup file; specify '-' to restore from stdin tar stream")
+	flags.StringVar(&cmd.Host, "host", "localhost:10101", "host:port of FeatureBase.")
+	flags.DurationVar(&cmd.RetryPeriod, "retry-period", cmd.RetryPeriod, "Length of time after HTTP request failure to continue retrying request.")
+	flags.StringVar(&cmd.Pprof, "pprof", cmd.Pprof, "host:port to listen for profiling requests at /debug/pprof and /debug/fgprof.")
+	flags.StringVar(&cmd.AuthToken, "auth-token", "", "Authentication token")
+	ctl.SetTLSConfig(
+		flags, "",
+		&cmd.TLS.CertificatePath,
+		&cmd.TLS.CertificateKeyPath,
+		&cmd.TLS.CACertPath,
+		&cmd.TLS.SkipVerify,
+		&cmd.TLS.EnableClientVerification,
+	)
+
+	return restoreCmd
+}
--- cmd/root.go
+++ cmd/root.go
@@ -53,6 +53,8 @@ at https://docs.molecula.cloud/.
 	rc.AddCommand(newChkSumCommand(stdin, stdout, stderr))
 	rc.AddCommand(newBackupCommand(stdin, stdout, stderr))
 	rc.AddCommand(newRestoreCommand(stdin, stdout, stderr))
+	rc.AddCommand(newBackupTarCommand(stdin, stdout, stderr))
+	rc.AddCommand(newRestoreTarCommand(stdin, stdout, stderr))
 	rc.AddCommand(newConfigCommand(stdin, stdout, stderr))
 	rc.AddCommand(newExportCommand(stdin, stdout, stderr))
 	rc.AddCommand(newGenerateConfigCommand(stdin, stdout, stderr))
