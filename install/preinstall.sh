0d7f676ce55cd250cf13cf0ba8b12319809e701f
Julio Martinez <julio.martinez@molecula.com>
2022-10-06 08:38:14 -0700

    Change featurebase user home path to /var/lib/featurebase. (#2242)

--- install/preinstall.sh
+++ install/preinstall.sh
@@ -2,7 +2,7 @@
 
 USERNAME=featurebase
 GROUPNAME=featurebase
-HOMEDIR=/var/lib/molecula
+HOMEDIR=/var/lib/featurebase
 
 getent group $GROUPNAME >/dev/null || groupadd -r $GROUPNAME
 getent passwd $USERNAME >/dev/null || \
