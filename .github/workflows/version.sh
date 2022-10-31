bed4f11d43c8c6a778ffff58dded5569923e6837
Christopher Lowenthal <christopher.lowenthal@molecula.com>
2022-10-31 16:58:42 -0400

    fixes index in version script

--- .github/workflows/version.sh
+++ .github/workflows/version.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 current=$(git describe --tags | grep -Eo '[0-9]*?\.[0-9]*?\.[0-9]*')
 array=($(echo $current | tr '.' '\n'))
-array[2]=$((array[2]+1))
+array[1]=$((array[1]+1))
 echo $(IFS="." ; echo "${array[*]}")
