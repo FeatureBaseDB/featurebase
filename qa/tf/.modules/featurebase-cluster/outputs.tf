5154639b3bc28054a4716113273ea78888259b39
Garrison Davis <garrison.davis@featurebase.com>
2022-11-07 16:04:09 -0700

    Make spot instance selection dynamic
    The idea behind this is to give AWS more information about what
    instances we can let it actually instantitate, rather than have it be
    one fixed instance type.
    
    e.g., in this case, we are okay with any Graviton instance with at least
    2 vCPU and 8 GiB memory.
    
    The easist way to do that is to instead use a launch template, with
    ec2_fleets or spot fleets.
    
    I took out the part where we even support on-demand instances. This can
    be readded later if it is necessary.
    
    (cherry picked from commit 3bfb65eb583132a7839b335f898d7bba003e7e2c)

--- qa/tf/.modules/featurebase-cluster/outputs.tf
+++ qa/tf/.modules/featurebase-cluster/outputs.tf
@@ -1,15 +1,30 @@
 output "ingest_ips" {
-    value = var.use_spot_instances ? aws_spot_instance_request.fb_ingest.*.private_ip : aws_instance.fb_ingest.*.private_ip
+  value = data.aws_instances.ingest_nodes.private_ips
 }
 
 output "data_node_ips" {
-    value = var.use_spot_instances ? aws_spot_instance_request.fb_cluster_nodes.*.private_ip : aws_instance.fb_cluster_nodes.*.private_ip 
+  value = data.aws_instances.data_nodes.private_ips
 }
 
 output "cluster_prefix" {
-    value = var.cluster_prefix
+  value = var.cluster_prefix
 }
 
 output "fb_cluster_replica_count" {
-    value = var.fb_cluster_replica_count
+  value = var.fb_cluster_replica_count
+}
+
+// Provide the ingest and data node information for module outputs
+data "aws_instances" "ingest_nodes" {
+  instance_tags = {
+    Prefix = var.cluster_prefix
+    Role   = "ingest_node"
+  }
+}
+
+data "aws_instances" "data_nodes" {
+  instance_tags = {
+    Prefix = var.cluster_prefix
+    Role   = "cluster_node"
+  }
 }
