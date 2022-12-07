de1ce7d86b1b585140b40f3634ada5ccc70f92bc
rachithrr <rachith.ramaswamy@molecula.com>
2022-12-08 04:01:30 +0530

    FB-1795: Implement REVERSE() scalar string function (#2335)
    (cherry picked from commit d33bf4811f9803ae626f88ecbd183fd804b4b76a)

---
+++ sql3/test/defs/defs_string_functions.go
@@ -0,0 +1,32 @@
+package defs
+
+// string function tests
+var stringScalarFunctionsTests = TableTest{
+
+	Table: tbl(
+		"stringscalarfunctions",
+		srcHdrs(
+			srcHdr("_id", fldTypeID),
+			srcHdr("a", fldTypeInt, "min 0", "max 1000"),
+			srcHdr("b", fldTypeInt, "min 0", "max 1000"),
+			srcHdr("ts", fldTypeTimestamp),
+		),
+		srcRows(
+			srcRow(int64(1), int64(10), int64(100), knownTimestamp()),
+		),
+	),
+	SQLTests: []SQLTest{
+		{
+			SQLs: sqls(
+				"select reverse('this')",
+			),
+			ExpHdrs: hdrs(
+				hdr("", fldTypeString),
+			),
+			ExpRows: rows(
+				row(string("siht")),
+			),
+			Compare: CompareExactUnordered,
+		},
+	},
+}
