12ff18bc5508f635a41d456e0e17279d0781d5fd
rachithrr <rachith.ramaswamy@molecula.com>
2022-12-09 05:20:15 +0530

    FB-1800: Implement SUBSTRING()  (#2343)
    substring(string, startIndex,length).
    
    (cherry picked from commit aa2a62fda05bf85a72030ebc9aeb8945e6c1e09a)

--- sql3/planner/inbuiltfunctionsstring.go
+++ sql3/planner/inbuiltfunctionsstring.go
@@ -1,6 +1,8 @@
 package planner
 
 import (
+	"strconv"
+
 	"github.com/molecula/featurebase/v3/sql3"
 	"github.com/molecula/featurebase/v3/sql3/parser"
 )
@@ -13,6 +15,27 @@ func (p *ExecutionPlanner) analyseFunctionReverse(call *parser.Call, scope parse
 
 	if !typeIsString(call.Args[0].DataType()) {
 		return nil, sql3.NewErrStringExpressionExpected(call.Args[0].Pos().Line, call.Args[0].Pos().Column)
+	}
+
+	call.ResultDataType = parser.NewDataTypeString()
+
+	return call, nil
+}
+
+func (p *ExecutionPlanner) analyseFunctionSubstring(call *parser.Call, scope parser.Statement) (parser.Expr, error) {
+	if len(call.Args) <= 1 || len(call.Args) > 3 {
+		return nil, sql3.NewErrCallParameterCountMismatch(call.Rparen.Line, call.Rparen.Column, call.Name.Name, 2, len(call.Args))
+	}
+
+	if !typeIsString(call.Args[0].DataType()) {
+		return nil, sql3.NewErrStringExpressionExpected(call.Args[0].Pos().Line, call.Args[0].Pos().Column)
+	}
+
+	// the third parameter is optional
+	for i := 1; i < len(call.Args); i++ {
+		if !typeIsInteger(call.Args[i].DataType()) {
+			return nil, sql3.NewErrIntExpressionExpected(call.Args[i].Pos().Line, call.Args[i].Pos().Column)
+		}
 	}
 
 	call.ResultDataType = parser.NewDataTypeString()
@@ -38,4 +61,48 @@ func (n *callPlanExpression) EvaluateReverse(currentRow []interface{}) (interfac
 		runes[i], runes[j] = runes[j], runes[i]
 	}
 	return string(runes), nil
+}
+
+// Takes string, startIndex and length and returns the substring.
+func (n *callPlanExpression) EvaluateSubstring(currentRow []interface{}) (interface{}, error) {
+	argOneEval, err := n.args[0].Evaluate(currentRow)
+	if err != nil {
+		return nil, err
+	}
+
+	stringArgOne, ok := argOneEval.(string)
+	if !ok {
+		return nil, sql3.NewErrInternalf("unexpected type converion %T", argOneEval)
+	}
+
+	// this takes a sliding window approach to evaluate substring.
+	startIndex, err := strconv.Atoi(n.args[1].String())
+	if err != nil {
+		return nil, sql3.NewErrInternalf("unexpected type converion %T", n.args[1])
+	}
+	if startIndex >= len(stringArgOne) {
+		return "", nil
+	}
+
+	endIndex := len(stringArgOne)
+	if len(n.args) > 2 {
+		ln, err := strconv.Atoi(n.args[2].String())
+		if err != nil {
+			return nil, sql3.NewErrInternalf("unexpected type converion %T", n.args[1])
+		}
+		endIndex = startIndex + ln
+	}
+	if endIndex < 0 {
+		return "", nil
+	}
+
+	if startIndex < 0 {
+		startIndex = 0
+	}
+
+	if endIndex > len(stringArgOne) {
+		return stringArgOne[startIndex:], nil
+	}
+
+	return stringArgOne[startIndex:endIndex], nil
 }
