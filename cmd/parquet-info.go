4e45f19ca0662651af2384b7eab13557a8c5dab7
tgruben <tgruben@gmail.com>
2023-01-26 12:55:54 -0600

    parquet-info command to browse parquet files (#2230)
    * parquet-info command to browse parquet files
    
    * null support in parquet

---
+++ cmd/parquet-info.go
@@ -0,0 +1,33 @@
+// Copyright 2022 Molecula Corp. (DBA FeatureBase).
+// SPDX-License-Identifier: Apache-2.0
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/featurebasedb/featurebase/v3/ctl"
+	"github.com/featurebasedb/featurebase/v3/logger"
+	"github.com/spf13/cobra"
+)
+
+func newParquetInfoCommand(logdest logger.Logger) *cobra.Command {
+	c := ctl.NewParquetInfoCommand(logdest)
+	cmd := &cobra.Command{
+		Use:   "parquet-info PATH|URL",
+		Short: "Inspect Parquet Files.",
+		Long: `
+Displays schema and sample data from the specified file
+`,
+		Args: func(cmd *cobra.Command, args []string) error {
+			if len(args) == 0 {
+				return fmt.Errorf("data directory path required")
+			} else if len(args) > 1 {
+				return fmt.Errorf("too many command line arguments")
+			}
+			c.Path = args[0]
+			return nil
+		},
+		RunE: usageErrorWrapper(c),
+	}
+	return cmd
+}
