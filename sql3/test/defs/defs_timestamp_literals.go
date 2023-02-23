afc50f231ea476560a7cad04c3b142872549da60
Vengata Krishnan <vkrishnan@featurebase.com>
2023-02-23 11:41:49 -0500

    Adding test coverage for new scalar function ToTimestamp() and implicit conversion int to timestamp.

--- sql3/test/defs/defs_timestamp_literals.go
+++ sql3/test/defs/defs_timestamp_literals.go
@@ -1,6 +1,8 @@
 // Copyright 2021 Molecula Corp. All rights reserved.
 package defs
 
+import "time"
+
 var timestampLiterals = TableTest{
 	Table: tbl(
 		"testtimestampliterals",
@@ -18,7 +20,7 @@ var timestampLiterals = TableTest{
 		{
 			// InsertWithCurrentTimestamp
 			SQLs: sqls(
-				"insert into testtimestampliterals (_id, a, b, d, ts, event, ievent) values (4, 40, 400, 10.12, current_timestamp, ['A', 'B', 'C'], [1, 2, 3])",
+				"insert into testtimestampliterals (_id, a, b, d, ts, event, ievent) values (1, 40, 400, 10.12, current_timestamp, ['A', 'B', 'C'], [1, 2, 3])",
 			),
 			ExpHdrs: hdrs(),
 			ExpRows: rows(),
@@ -27,10 +29,33 @@ var timestampLiterals = TableTest{
 		{
 			// InsertWithCurrentDate
 			SQLs: sqls(
-				"insert into testtimestampliterals (_id, a, b, d, ts, event, ievent) values (4, 40, 400, 10.12, current_date, ['A', 'B', 'C'], [1, 2, 3])",
+				"insert into testtimestampliterals (_id, a, b, d, ts, event, ievent) values (2, 40, 400, 10.12, current_date, ['A', 'B', 'C'], [1, 2, 3])",
 			),
 			ExpHdrs: hdrs(),
 			ExpRows: rows(),
+			Compare: CompareExactUnordered,
+		},
+		{
+			// Insert literal 0 into a timestamp, it should be stored as 1970-01-01 00:00:00 +0000 UTC (unix epoch base value)
+			SQLs: sqls(
+				"insert into testtimestampliterals (_id, a, b, d, ts, event, ievent) values (3, 40, 400, 10.12, 0, ['A', 'B', 'C'], [1, 2, 3])",
+			),
+			ExpHdrs: hdrs(),
+			ExpRows: rows(),
+			Compare: CompareExactUnordered,
+		},
+		{
+			SQLs: sqls(
+				"select datepart('yy', ts) as \"yy\" from testtimestampliterals",
+			),
+			ExpHdrs: hdrs(
+				hdr("yy", fldTypeInt),
+			),
+			ExpRows: rows(
+				row(int64(1970)),
+				row(int64(time.Now().UTC().Year())),
+				row(int64(time.Now().UTC().Year())),
+			),
 			Compare: CompareExactUnordered,
 		},
 	},
