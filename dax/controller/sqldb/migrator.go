b2610d7ea32dab5d7487bd5516be2e9ca6f0db34
Travis Turner <travis@molecula.com>
2023-03-30 14:26:54 -0500

    Migration for directive version by address

--- dax/controller/sqldb/migrator.go
+++ dax/controller/sqldb/migrator.go
@@ -58,39 +58,41 @@ func NewEmbedMigrator(fs fs.FS, c *pop.Connection, log logger.Logger) (*EmbedMig
 
 func (fm *EmbedMigrator) findMigrations(runner func(mf pop.Migration, tx *pop.Connection) error) error {
 	return fs.WalkDir(fm.FS, "migrations", func(path string, d fs.DirEntry, err error) error {
-		fmt.Printf("walking path: %s, d: %v, err: %v\n", path, d, err)
-		if !d.IsDir() {
-			match, err := pop.ParseMigrationFilename(d.Name())
-			if err != nil {
-				if strings.HasPrefix(err.Error(), "unsupported dialect") {
-					fm.log.Warnf("ignoring migration file with %s", err.Error())
-					return nil
-				}
-				return err
-			}
-			if match == nil {
-				fm.log.Warnf("ignoring file %s because it does not match the migration file pattern", d.Name())
+		if d.IsDir() {
+			return nil
+		}
+
+		match, err := pop.ParseMigrationFilename(d.Name())
+		if err != nil {
+			if strings.HasPrefix(err.Error(), "unsupported dialect") {
+				fm.log.Warnf("ignoring migration file with %s", err.Error())
 				return nil
 			}
-			mf := pop.Migration{
-				Path:      path,
-				Version:   match.Version,
-				Name:      match.Name,
-				DBType:    match.DBType,
-				Direction: match.Direction,
-				Type:      match.Type,
-				Runner:    runner,
-			}
-			switch mf.Direction {
-			case "up":
-				fm.UpMigrations.Migrations = append(fm.UpMigrations.Migrations, mf)
-			case "down":
-				fm.DownMigrations.Migrations = append(fm.DownMigrations.Migrations, mf)
-			default:
-				// the regex only matches `(up|down)` for direction, so a panic here is appropriate
-				panic("got unknown migration direction " + mf.Direction)
-			}
+			return err
 		}
+		if match == nil {
+			fm.log.Warnf("ignoring file %s because it does not match the migration file pattern", d.Name())
+			return nil
+		}
+		mf := pop.Migration{
+			Path:      path,
+			Version:   match.Version,
+			Name:      match.Name,
+			DBType:    match.DBType,
+			Direction: match.Direction,
+			Type:      match.Type,
+			Runner:    runner,
+		}
+		switch mf.Direction {
+		case "up":
+			fm.UpMigrations.Migrations = append(fm.UpMigrations.Migrations, mf)
+		case "down":
+			fm.DownMigrations.Migrations = append(fm.DownMigrations.Migrations, mf)
+		default:
+			// the regex only matches `(up|down)` for direction, so a panic here is appropriate
+			panic("got unknown migration direction " + mf.Direction)
+		}
+
 		return nil
 	})
 }
